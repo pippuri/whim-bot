b87c9fd2cb557136e9534e5038e482dcc474d525
Kenneth Falck <kennu@iki.fi>
2016-03-15 01:16:16 +0200

    Switch to TripGo v11 query format

--- provider-tripgo/routes/index.js
+++ provider-tripgo/routes/index.js
@@ -3,29 +3,40 @@ var request = require('request-promise');
 
 var TRIPGO_SOUTH_FINLAND_ROUTING_URL = 'https://hadron-fi-southfinland.tripgo.skedgo.com/satapp/routing.json';
 
+var TRIPGO_MODES = [
+  "pt_pub"
+  //"ps_tax",
+  //"me_car",
+  //"me_car-s_Ekorent",
+  //"me_mot",
+  //"cy_bic",
+  //"wa_wal",
+];
+
+// Docs: http://planck.buzzhives.com/swagger/index.html#!/Routing/get_routing_json
+
 function getTripGoRoutes(from, to) {
+  var qs = {
+    v: '11',
+    from: '(' + from + ')',
+    to: '(' + to + ')',
+    departAfter: Math.floor(Date.now()/1000)+3600*10,
+    arriveBefore: '0',
+    modes: TRIPGO_MODES.join(','), // modes to use in routing, separated by commas (get from regions API)
+    avoid: '', // public transport modes to avoid, separated by commas
+    tt: '3', // preferred transfer time in minutes
+    ws: '1', // walking speed (0=slow 1=medium 2=fast)
+    cs: '1', // cycling speed (0=slow 1=medium 2=fast)
+    wp: '(1, 1, 1, 1)', // weights for price, environmental impact, duration, and convenience between 0.1..2.0
+    ir: 'true' // interregional results
+  };
+  console.log(qs);
   return request.get(TRIPGO_SOUTH_FINLAND_ROUTING_URL, {
     json: true,
     headers: {
       'X-TripGo-Key': process.env.TRIPGO_API_KEY
     },
-    qs: {
-      from: '(' + from + ')',
-      to: '(' + to + ')',
-      arriveBefore: '0',
-      departAfter: Math.floor(Date.now()/1000),
-      wp: '(1.0,1.0,1.0,1.0)',
-      unit: 'metric',
-      vehiclesFlag: '00010',
-      bestOnly: 'false',
-      version: 'aTripGo',
-      v: '6',
-      tt: '0',
-      ws: '1',
-      cs: '1',
-      ss: 'false',
-      es: 'false'
-    }
+    qs: qs
   });
 }
 
