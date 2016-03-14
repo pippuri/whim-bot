e895f89a0484f2f7c804c088961261f99e6250ce
Kenneth Falck <kennu@iki.fi>
2016-03-15 01:48:50 +0200

    Use v11 TripGo format in mock UI

--- www/dev.maas.global/client/app/main/main.controller.js
+++ www/dev.maas.global/client/app/main/main.controller.js
@@ -4,8 +4,9 @@
 
 class MainController {
 
-  constructor($http) {
+  constructor($http, $filter) {
     this.$http = $http;
+    this.$filter = $filter;
     this.from = '60.185034,24.9147957';
     this.to = '60.1883726,24.9574861';
     this.providers = ['tripgo', 'here'];
@@ -28,9 +29,11 @@ class MainController {
     var template = this.segmentTemplates[segment.segmentTemplateHashCode];
     var duration = segment.endTime - segment.startTime;
     var durationMinutes = ' (' + Math.floor(duration / 60) + ' min) ';
+    var time = this.$filter('date')(segment.startTime*1000, 'HH:mm');
     var text = template.action
     .replace(/<DURATION>/g, durationMinutes)
-    .replace(/<NUMBER>/g, segment.serviceNumber);
+    .replace(/<NUMBER>/g, segment.serviceNumber)
+    .replace(/<TIME>/g, time);
     if (template.from && template.from.address) {
       text += ' from ' + template.from.address;
     }
