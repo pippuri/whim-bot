d6dbd1a7c109c1e7f7a66a28c9b1a85490017c70
Kenneth Falck <kennu@iki.fi>
2016-03-15 20:20:46 +0200

    Clean up route viewer

--- www/dev.maas.global/client/app/main/main.controller.js
+++ www/dev.maas.global/client/app/main/main.controller.js
@@ -21,28 +21,7 @@ class MainController {
     this.$http.get(this.directUrl)
     .then((response) => {
       this.routes = response.data;
-      this.routes.segmentTemplates.map((template) => {
-        this.segmentTemplates[template.hashCode] = template;
-      });
     });
-  }
-
-  expandSegmentTemplate(segment) {
-    var template = this.segmentTemplates[segment.segmentTemplateHashCode];
-    var duration = segment.endTime - segment.startTime;
-    var durationMinutes = ' (' + Math.floor(duration / 60) + ' min) ';
-    var time = this.$filter('date')(segment.startTime*1000, 'HH:mm');
-    var text = template.action
-    .replace(/<DURATION>/g, durationMinutes)
-    .replace(/<NUMBER>/g, segment.serviceNumber)
-    .replace(/<TIME>/g, time);
-    if (template.from && template.from.address) {
-      text += ' from ' + template.from.address;
-    }
-    if (template.to && template.to.address) {
-      text += ' to ' + template.to.address;
-    }
-    return text;
   }
 }
 
