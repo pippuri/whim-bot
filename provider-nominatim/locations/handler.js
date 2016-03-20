966d977ccc533e1134e2e554df152f5ce6836cb4
Lauri Svan <lauri.svan@sc5.io>
2016-03-20 22:30:21 +0200

    First version of Nominatim provider

---
+++ provider-nominatim/locations/handler.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Require Serverless ENV vars
+var ServerlessHelpers = require('serverless-helpers-js').loadEnv();
+
+// Require Logic
+var lib = require('./index.js');
+
+// Lambda Handler
+module.exports.handler = function(event, context) {
+
+  lib.respond(event, function(error, response) {
+    return context.done(error, response);
+  });
+};
---
+++ provider-nominatim/locations/index.js
@@ -0,0 +1,94 @@
+var Promise = require('bluebird');
+var request = require('request-promise');
+var util = require('util');
+
+var ENDPOINT_URL = 'http://nominatim.openstreetmap.org/search';
+
+function adapt(input) {
+  // Customise query by the hints given
+  var query = {
+      format: 'json',
+      addressdetails: 1,
+      limit: input.count,
+      q: input.name,
+  };
+
+  switch(input.hint) {
+    case 'latlon':
+      // In absence of full circle, use bounding box. Use
+      // http://stackoverflow.com/questions/1253499/simple-calculations-for-working-with-lat-lon-km-distance
+      // Latitude: 1 deg = 110.574 km
+      // Longitude: 1 deg = 111.320*cos(latitude) km
+      // for conversion rule
+      var latRadians = input.lat * Math.PI / 180;
+      var lonRadians = input.lon * Math.PI / 180;
+      var left = input.lat - input.radius / 111.320 * Math.cos(latRadians);
+      var top = input.lon - input.radius / 110.574;
+      var right = input.lat + input.radius / 111.320 * Math.cos(latRadians);
+      var bottom = input.lon + input.radius / 110.574;
+
+      query.viewbox = [ left, top, right, bottom ].join(',')
+      break;
+    case 'country':
+      query.country = input.country;
+      if (typeof input.city !== 'undefined') {
+        query.city = input.city;
+      }
+      break;
+    case 'none':
+      break;
+    default:
+      throw new Error('Location hint not given');
+  }
+
+  return request.get(ENDPOINT_URL, {
+    json: true,
+    headers: {},
+    qs: query
+  })
+  .then(parseResults)
+  .then(function (locations) {
+    return {
+      locations: locations,
+      query: query
+    };
+  });
+}
+
+function parseResults(response) {
+  console.log(response);
+  var locations = [];
+
+  if (!util.isArray(response)) {
+    var error = new Error('Invalid response from HERE - invalid format.');
+    return Promise.reject(error);
+  }
+
+  response.forEach(function(item) {
+    console.log(item);
+
+    var location = {
+      name: item.display_name,
+      lat: item.lat,
+      lon: item.lon,
+      zipCode: item.address.postcode,
+      city: item.address.city,
+      country: item.address.country,
+      address: item.address.construction
+    };
+
+    locations.push(location);
+  });
+
+  return Promise.resolve(locations);
+}
+
+module.exports.respond = function (event, callback) {
+  adapt(event)
+  .then(function(response) {
+    callback(null, response);
+  })
+  .catch(function(err) {
+    callback(err);
+  });
+};
