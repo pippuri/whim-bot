38df73131274b8f45c88add86acab47f199f8221
Kenneth Falck <kennu@iki.fi>
2016-03-24 01:18:12 +0200

    MQTT subscriptions in Angular

--- www/dev.maas.global/client/app/mqtt/mqtt.controller.js
+++ www/dev.maas.global/client/app/mqtt/mqtt.controller.js
@@ -4,11 +4,13 @@
 
 class MqttController {
 
-  constructor($http, $localStorage, $timeout, API_BASE_URL) {
+  constructor($scope, $http, $localStorage, $timeout, API_BASE_URL) {
+    this.$scope = $scope;
     this.$http = $http;
     this.$localStorage = $localStorage;
     this.$timeout = $timeout;
     this.API_BASE_URL = API_BASE_URL;
+    this.messageLog = [];
 
     this.options = {
       regionName: 'eu-west-1',
@@ -25,16 +27,18 @@ class MqttController {
       }
     })
     .then((response) => {
+      var cleanId = response.data.IdentityId.replace(/:/g, '-');
       this.options.clientId = 'maas-client-' + response.data.IdentityId + '-' + Date.now();
       this.options.topicFilter = 'maas/id/' + response.data.IdentityId + '/#';
       this.options.accessKey = response.data.Credentials.AccessKeyId;
       this.options.secretKey = response.data.Credentials.SecretKey;
       this.options.sessionToken = response.data.Credentials.SessionToken;
       console.log('Opening MQTT client connection as', this.options);
+      this.messageLog.push({msg:'Connecting to MQTT service...'});
       this.client = new Messaging.Client(this.options.endpoint, 443, this.options.clientId);
       this.client.onConnectionLost = this.onConnectionLost.bind(this);
       this.client.onMessageArrived = this.onMessageArrived.bind(this);
-      this.client.connect({onSuccess:this.onConnect.bind(this), useSSL:true, path:SigV4Utils.signedMqttPath(this.options)});
+      this.client.connect({onSuccess:this.onConnect.bind(this), onFailure:this.onFailure.bind(this), useSSL:true, path:SigV4Utils.signedMqttPath(this.options)});
     })
     .then(null, (err) => {
       this.error = err.data && err.data.errorMessage || err;
@@ -42,29 +46,44 @@ class MqttController {
   }
 
   onConnect() {
-    this.connected = true;
+    this.messageLog.push({msg:'Connected to MQTT service.'});
+    console.log('CONNECTED');
     console.log('Subscribing to topic', this.options.topicFilter);
+    this.messageLog.push({msg:'Subscribing to topic ' + this.options.topicFilter});
+    this.connected = true;
     this.client.subscribe(this.options.topicFilter);
+    this.$scope.$apply();
+  }
+
+  onFailure(err) {
+    console.log('CONNECT FAILURE', err);
+    this.messageLog.push({msg:'Connection to MQTT service failed: ' + err});
+    this.$scope.$apply();
   }
 
   onConnectionLost(responseObject) {
+    console.log('DISCONNECTED');
+    this.messageLog.push({msg:'Disconnected from MQTT service.'});
     this.connected = false;
     if (!this.client) return;
     var self = this;
     if (responseObject.errorCode !== 0) {
       // Reconnect after a delay
-      $timeout(function () {
-        client.connect({onSuccess:self.onConnect.bind(self), useSSL:true, path:SigV4Utils.signedMqttPath(self.options)});
+      console.log('Got MQTT error', responseObject.errorCode, 'reconnecting in 10 seconds');
+      this.$timeout(function () {
+        self.client.connect({onSuccess:self.onConnect.bind(self), useSSL:true, path:SigV4Utils.signedMqttPath(self.options)});
       }, 10000);
     } else {
       // Reconnect immediately
-      client.connect({onSuccess:self.onConnect.bind(self), useSSL:true, path:SigV4Utils.signedMqttPath(self.options)});
+      self.client.connect({onSuccess:self.onConnect.bind(self), useSSL:true, path:SigV4Utils.signedMqttPath(self.options)});
     }
+    this.$scope.$apply();
   }
 
   onMessageArrived(message) {
-    var payload = JSON.parse(message.payloadString);
-    console.log("onMessageArrived:", message.destinationName, payload);
+    console.log("onMessageArrived:", message.destinationName, message.payloadString);
+    this.messageLog.push({msg:'[' + message.destinationName + '] ' + message.payloadString});
+    this.$scope.$apply();
   }
 }
 
