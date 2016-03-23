4f5190be83974d7cd604039e32e8ab8caf32f265
Kenneth Falck <kennu@iki.fi>
2016-03-23 03:42:30 +0200

    Implemented basic SHA1 based SMS code check, Cognito profile and JWT ID tokens

--- auth/sms-login/index.js
+++ auth/sms-login/index.js
@@ -1,16 +1,118 @@
 var Promise = require('bluebird');
+var crypto = require('crypto');
 var AWS = require('aws-sdk');
+var jwt = require('jsonwebtoken');
+
+var cognitoIdentity = new AWS.CognitoIdentity({region:process.env.AWS_REGION});
+var cognitoSync = new AWS.CognitoSync({region:process.env.AWS_REGION});
+
+Promise.promisifyAll(cognitoIdentity);
+Promise.promisifyAll(cognitoSync);
 
 /**
- * Perform an OAuth2 login using a verification code (or link) sent by SMS.
+ * Create or retrieve Amazon Cognito identity.
  */
-function smsLogin(phoneNumber, verificationCode) {
-  return Promise.resolve({
+function getCognitoDeveloperIdentity(plainPhone) {
+  // Use sha1 hash of subscriberId because it may contain spaces, which are not allowed in Cognito
+  var logins = {};
+  logins[process.env.COGNITO_DEVELOPER_PROVIDER] = 'tel:' + plainPhone;
+  var options = {
+    IdentityPoolId: process.env.COGNITO_POOL_ID,
+    Logins: logins
+  };
+  console.log('Getting cognito developer identity with', JSON.stringify(options, null, 2));
+  return cognitoIdentity.getOpenIdTokenForDeveloperIdentityAsync(options)
+  .then(function (response) {
+    return response.IdentityId;
+  });
+}
+
+/**
+ * Create or update Amazon Cognito profile dataset.
+ */
+function updateCognitoProfile(principalId, profile) {
+  var syncSessionToken;
+  var patches = [];
+  return cognitoSync.listRecordsAsync({
+    IdentityPoolId: process.env.COGNITO_POOL_ID,
+    IdentityId: principalId,
+    DatasetName: process.env.COGNITO_PROFILE_DATASET,
+  })
+  .then(function (response) {
+    syncSessionToken = response.SyncSessionToken;
+    var oldRecords = {};
+    response.Records.map(function (record) {
+      oldRecords[record.Key] = record;
+    });
+    Object.keys(profile).map(function (key) {
+      var oldRecord = oldRecords[key];
+      var newValue;
+      if (typeof profile[key] == 'object') {
+        newValue = JSON.stringify(profile[key]);
+      } else {
+        newValue = ''+profile[key];
+      }
+      // Check if changed
+      if (!oldRecord || newValue != oldRecord.Value) {
+        patches.push({
+          Op: 'replace',
+          Key: key,
+          Value: newValue,
+          SyncCount: oldRecord ? oldRecord.SyncCount : 0
+        });
+      }
+    });
+    if (patches.length > 0) {
+      return cognitoSync.updateRecordsAsync({
+        IdentityPoolId: process.env.COGNITO_POOL_ID,
+        IdentityId: principalId,
+        DatasetName: process.env.COGNITO_PROFILE_DATASET,
+        SyncSessionToken: syncSessionToken,
+        RecordPatches: patches
+      });
+    }
+  });
+}
+
+/**
+ * Login using a verification code sent by SMS.
+ */
+function smsLogin(phone, code) {
+  var plainPhone = phone.replace(/[^\d]/g, '');
+  if (!plainPhone || plainPhone.length < 4) {
+    return Promise.reject(new Error('Invalid phone number'));
+  }
+  var shasum = crypto.createHash('sha1');
+  var salt = code.slice(0, 3);
+  shasum.update(salt + process.env.SMS_CODE_SECRET + plainPhone);
+  var hash = shasum.digest('hex');
+  var correctCode = salt + '' + (100+parseInt(hash.slice(0, 3), 16));
+  console.log('Verifying SMS code', code, 'for', phone, 'plainphone', plainPhone, 'correct', correctCode);
+  if (correctCode !== code) {
+    return Promise.reject(new Error('401 Unauthorized'));
+  }
+  var principalId;
+  return getCognitoDeveloperIdentity(plainPhone)
+  .then(function (identityId) {
+    principalId = identityId;
+    return updateCognitoProfile(principalId, {
+      phone: phone,
+      verificationCode: code
+    });
+  })
+  .then(function () {
+    // Create a signed JSON web token
+    var token = jwt.sign({
+      id: principalId
+    }, process.env.JWT_SECRET);
+    return {
+      id_token: token
+    };
   });
 }
 
 module.exports.respond = function (event, callback) {
-  smsLogin(event.phoneNumber, event.verificationCode)
+  smsLogin(''+event.phone, ''+event.code)
   .then(function (response) {
     callback(null, response);
   })
--- auth/sms-request-code/index.js
+++ auth/sms-request-code/index.js
@@ -1,4 +1,5 @@
 var Promise = require('bluebird');
+var crypto = require('crypto');
 var AWS = require('aws-sdk');
 var lambda = new AWS.Lambda({region:process.env.AWS_REGION});
 Promise.promisifyAll(lambda, {suffix:'Promise'});
@@ -7,12 +8,22 @@ Promise.promisifyAll(lambda, {suffix:'Promise'});
  * Request a login verification code by SMS.
  */
 function smsRequestCode(phone, provider) {
-  if (!provider) {
+  if (!provider || provider == 'undefined') {
     provider = 'twilio';
   }
-  var verificationCode = 1000 + Math.floor(Math.random() * 9000);
+  // Clean up phone number to only contain digits
+  var plainPhone = phone.replace(/[^\d]/g, '');
+  if (!plainPhone || plainPhone.length < 4) {
+    return Promise.reject(new Error('Invalid phone number'));
+  }
+  var shasum = crypto.createHash('sha1');
+  var salt = '' + (100 + Math.floor(Math.random() * 900));
+  shasum.update(salt + process.env.SMS_CODE_SECRET + plainPhone);
+  var hash = shasum.digest('hex');
+  var verificationCode = salt + '' + (100+parseInt(hash.slice(0, 3), 16));
   var verificationLink = process.env.WWW_BASE_URL + '/login?phone=' + encodeURIComponent(phone) + '&code=' + encodeURIComponent(verificationCode);
   var functionName = 'MaaS-provider-' + provider + '-send-sms';
+  console.log('Sending SMS verification code', verificationCode, 'to', phone, 'with link', verificationLink, 'plainphone', plainPhone);
   return lambda.invokePromise({
     FunctionName: functionName,
     Qualifier: process.env.SERVERLESS_STAGE.replace(/^local$/, 'dev'),
@@ -31,7 +42,7 @@ function smsRequestCode(phone, provider) {
 }
 
 module.exports.respond = function (event, callback) {
-  smsRequestCode(event.phone, event.provider)
+  smsRequestCode(''+event.phone, ''+event.provider)
   .then(function (response) {
     callback(null, response);
   })
