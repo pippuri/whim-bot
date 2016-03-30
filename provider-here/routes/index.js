abe91dab2ac6e5af3cf6f49d09e0c13c5c8d1c91
Musie Kebede <gizaw@neocard.fi>
2016-03-30 13:43:38 +0300

    space 2 formatting applied

--- provider-here/routes/index.js
+++ provider-here/routes/index.js
@@ -5,35 +5,35 @@ var adapter = require('./adapter');
 var HERE_ROUTE_URL = 'https://route.cit.api.here.com/routing/7.2/calculateroute.json';
 
 function getHereRoutes(from, to, format) {
-    return request.get(HERE_ROUTE_URL, {
-            json: true,
-            headers: {},
-            qs: {
-                app_id: process.env.HERE_APP_ID,
-                app_code: process.env.HERE_APP_CODE,
-                waypoint0: 'geo!' + from,
-                waypoint1: 'geo!' + to,
-                departure: 'now',
-                mode: 'fastest;publicTransport',
-                combineChange: 'true',
-                maneuverAttributes: 'shape,roadName,nextRoadName'
-            }
-        })
-        .then(function (result) {
-            if (format == 'original') {
-                return result;
-            } else {
-                return adapter(result);
-            }
-        });
+  return request.get(HERE_ROUTE_URL, {
+    json: true,
+    headers: {},
+    qs: {
+      app_id: process.env.HERE_APP_ID,
+      app_code: process.env.HERE_APP_CODE,
+      waypoint0: 'geo!' + from,
+      waypoint1: 'geo!' + to,
+      departure: 'now',
+      mode: 'fastest;publicTransport',
+      combineChange: 'true',
+      maneuverAttributes: 'shape,roadName,nextRoadName'
+    }
+  })
+    .then(function (result) {
+    if (format == 'original') {
+      return result;
+    } else {
+      return adapter(result);
+    }
+  });
 }
 
 module.exports.respond = function (event, callback) {
-    getHereRoutes(event.from, event.to, event.format)
-        .then(function (response) {
-            callback(null, response);
-        })
-        .then(null, function (err) {
-            callback(err);
-        });
+  getHereRoutes(event.from, event.to, event.format)
+    .then(function (response) {
+    callback(null, response);
+  })
+    .then(null, function (err) {
+    callback(err);
+  });
 };
--- provider-tripgo/lib/routing.js
+++ provider-tripgo/lib/routing.js
@@ -20,64 +20,64 @@ var TRIPGO_TAXI_MODES = [
 // Docs: http://planck.buzzhives.com/swagger/index.html#!/Routing/get_routing_json
 
 function getTripGoRoutes(baseUrl, from, to, modes) {
-    var qs = {
-        v: '11',
-        from: '(' + from + ')',
-        to: '(' + to + ')',
-        departAfter: Math.floor(Date.now() / 1000),
-        arriveBefore: '0',
-        avoid: '', // public transport modes to avoid, separated by commas
-        tt: '3', // preferred transfer time in minutes
-        ws: '1', // walking speed (0=slow 1=medium 2=fast)
-        cs: '1', // cycling speed (0=slow 1=medium 2=fast)
-        wp: '(1, 1, 1, 1)', // weights for price, environmental impact, duration, and convenience between 0.1..2.0
-        ir: 'true', // interregional results
-        modes: modes // modes to use in routing, as an array
-    };
-    console.log(qs);
-    return request.get(baseUrl, {
-            json: true,
-            headers: {
-                'X-TripGo-Key': process.env.TRIPGO_API_KEY
-            },
-            qs: qs,
-            useQuerystring: true
-        })
-        .then(function (result) {
-            if (result.error) {
-                return Promise.reject(new Error(result.error));
-            } else {
-                return result;
-            }
-        });
+  var qs = {
+    v: '11',
+    from: '(' + from + ')',
+    to: '(' + to + ')',
+    departAfter: Math.floor(Date.now()/1000),
+    arriveBefore: '0',
+    avoid: '', // public transport modes to avoid, separated by commas
+    tt: '3', // preferred transfer time in minutes
+    ws: '1', // walking speed (0=slow 1=medium 2=fast)
+    cs: '1', // cycling speed (0=slow 1=medium 2=fast)
+    wp: '(1, 1, 1, 1)', // weights for price, environmental impact, duration, and convenience between 0.1..2.0
+    ir: 'true', // interregional results
+    modes: modes // modes to use in routing, as an array
+  };
+  console.log(qs);
+  return request.get(baseUrl, {
+    json: true,
+    headers: {
+      'X-TripGo-Key': process.env.TRIPGO_API_KEY
+    },
+    qs: qs,
+    useQuerystring: true
+  })
+  .then(function (result) {
+    if (result.error) {
+      return Promise.reject(new Error(result.error));
+    } else {
+      return result;
+    }
+  });
 }
 
 function getCombinedTripGoRoutes(baseUrl, from, to, format) {
-    return Promise.all([
+  return Promise.all([
     getTripGoRoutes(baseUrl, from, to, TRIPGO_PUBLIC_MODES),
     getTripGoRoutes(baseUrl, from, to, TRIPGO_TAXI_MODES)
   ])
-        .then(function (results) {
-            //console.log('Full results:', JSON.stringify(results, null, 2));
-            console.log('Results', results[0].groups.length, results[1].groups.length);
-            var response = results[0];
-            if (results[1] && results[1].groups) {
-                results[1].groups.map(function (group) {
-                    response.groups.push(group);
-                });
-            }
-            if (results[1] && results[1].segmentTemplates) {
-                results[1].segmentTemplates.map(function (segmentTemplate) {
-                    response.segmentTemplates.push(segmentTemplate);
-                });
-            }
-            console.log('Final Results', response.groups.length, format);
-            if (format == 'original') {
-                return response;
-            } else {
-                return adapter(response);
-            }
-        });
+  .then(function (results) {
+    //console.log('Full results:', JSON.stringify(results, null, 2));
+    console.log('Results', results[0].groups.length, results[1].groups.length);
+    var response = results[0];
+    if (results[1] && results[1].groups) {
+      results[1].groups.map(function (group) {
+        response.groups.push(group);
+      });
+    }
+    if (results[1] && results[1].segmentTemplates) {
+      results[1].segmentTemplates.map(function (segmentTemplate) {
+        response.segmentTemplates.push(segmentTemplate);
+      });
+    }
+    console.log('Final Results', response.groups.length, format);
+    if (format == 'original') {
+      return response;
+    } else {
+      return adapter(response);
+    }
+  });
 }
 
 module.exports.getCombinedTripGoRoutes = getCombinedTripGoRoutes;
