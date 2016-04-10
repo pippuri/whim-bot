8ef3a349a17c893175977da39692c89a110bc159
Toni Ruottu <toni.ruottu@sc5.io>
2016-04-10 17:11:25 +0300

    Clean up routes interface tests

--- test/routes-interface/feature-leave-at.js
+++ test/routes-interface/feature-leave-at.js
@@ -2,25 +2,24 @@
 var expect = require('chai').expect;
 var moment = require('moment');
 
+var validator = require('./validator');
+
 module.exports = function(handler) {
 
   describe('leaveAt request', function() {
 
-    var from = '60.1684126,24.9316739'; // SC5 Office
-    var to = '60.170779,24.7721584'; // Gallows Bird Pub
-    var leaveAt = moment().isoWeekday(8).hour(17).valueOf(); // Monday one week forward around five
-
     var early_margin = 58; // minutes (< 60 to make sure we catch time zone problems)
+
+    var event = {
+      from: '60.1684126,24.9316739', // SC5 Office
+      to: '60.170779,24.7721584', // Gallows Bird Pub
+      leaveAt: '' + moment().isoWeekday(8).hour(17).valueOf() // Monday one week forward around five
+    }
 
     var error;
     var response;
 
     before(function(done) {
-      var event = {
-        from: from,
-        to: to,
-        leaveAt: '' + leaveAt
-      }
       handler(event, {
         done: function(e, r) {
           error = e;
@@ -30,31 +29,25 @@ module.exports = function(handler) {
       });
     });
 
-    it('should succeed', function () {
+    it('should succeed without errors', function () {
       expect(error).to.be.null;
     });
-    it('should get response', function () {
-      expect(response).to.be.an('object');
-    });
-    it('response should be valid', function () {
-      var ajvFactory = require('ajv');
-      var schema = require('./schema.json');
-      var ajv = ajvFactory();
-      var validate = ajv.compile(schema);
-      var valid = validate(response);
-      var validation_error = valid ? null : JSON.stringify(validate.errors);
+    it('should trigger a valid response', function () {
+      var validation_error = validator(response);
       expect(validation_error).to.be.null;
     });
+
     it('response should have route', function () {
       expect(response.plan.itineraries.length).to.not.be.empty;
     });
     it('response route suggestions should be max ' + early_margin + ' minutes early', function () {
       response.plan.itineraries.forEach(function(i) {
-          var early_ms = (leaveAt - parseInt(i.startTime, 10));
+          var early_ms = (parseInt(event.leaveAt, 10) - parseInt(i.startTime, 10));
           var early_s = early_ms / 1000;
           var early_m = Math.floor(early_s / 60);
           expect(early_m).to.be.below(early_margin);
       });
     });
+
   });
 }
---
+++ test/routes-interface/validator.js
@@ -0,0 +1,11 @@
+var ajvFactory = require('ajv');
+
+var schema = require('./schema.json');
+
+module.exports = function(response) {
+  var ajv = ajvFactory();
+  var validate = ajv.compile(schema);
+  var valid = validate(response);
+  var validation_error = valid ? null : JSON.stringify(validate.errors);
+  return validation_error
+}
