431a6cfaba33c408708a12bb28961aea2d8458a4
Kenneth Falck <kennu@iki.fi>
2016-04-12 00:00:44 +0300

    Add API documentation website https://apidocs.maas.global

---
+++ swagger/swagger-maas-api-json/index.js
@@ -0,0 +1,19 @@
+var Promise = require('bluebird');
+var fs = require('fs');
+var path = require('path');
+var swaggerJson = JSON.parse(fs.readFileSync(path.join(__dirname, 'maas-api.json')));
+
+// Respond with Swagger API JSON
+function getApiJson() {
+  return Promise.resolve(swaggerJson);
+}
+
+module.exports.respond = function (event, callback) {
+  getApiJson()
+  .then(function (response) {
+    callback(null, response);
+  })
+  .catch(function (err) {
+    callback(err);
+  });
+};
