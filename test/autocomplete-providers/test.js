e9dc0bc3953f9850ff022dbf433b7e67bc9622d5
Musie Kebede <gizaw@neocard.fi>
2016-04-12 11:13:53 +0300

    Revise describe naming

--- test/autocomplete-providers/test.js
+++ test/autocomplete-providers/test.js
@@ -2,13 +2,13 @@ var testAutocomplete = require('./feature-query.js');
 
 describe('autocomplete provider',function(){
 
-  describe('Google places' , function(){
-      this.timeout(20000);
-      var lambda = require('../../provider-google/autocomplete/handler.js');
-      testAutocomplete(lambda);
+  describe('Google Places' , function(){
+    this.timeout(20000);
+    var lambda = require('../../provider-google/autocomplete/handler.js');
+    testAutocomplete(lambda);
   });
 
-  describe('HERE suggest', function () {
+  describe('Here', function () {
     this.timeout(20000);
     var lambda = require('../../provider-here/autocomplete/handler.js');
     testAutocomplete(lambda);
