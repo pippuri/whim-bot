e5664e97443df5d9488cbc334231aee9b468c4af
Toni Ruottu <toni.ruottu@sc5.io>
2016-04-12 19:10:38 +0300

    Remove get-deps dependency for running tests

--- gulpfile.js
+++ gulpfile.js
@@ -7,31 +7,19 @@ var mocha = require('gulp-mocha');
 
 // the two dependency retrievers fail if combined to one
 
-gulp.task('get-main-deps-async', function () {
+gulp.task('get-main-deps', function () {
   gulp.src(['package.json'])
     .pipe(install());
 });
 
 // the two dependency retrievers fail if combined to one
 
-gulp.task('get-component-deps-async', function () {
+gulp.task('get-component-deps', function () {
   gulp.src(['*/package.json'])
     .pipe(install());
 });
 
-gulp.task('get-deps-async', ['get-main-deps-async', 'get-component-deps-async']);
-
-// gulp-install does not block as expected, so we need to do it ourselves through exec
-
-gulp.task('get-deps', function (callback) {
-  exec('gulp get-deps-async', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    callback(err);
-  });
-});
-
-gulp.task('build', ['get-deps']);
+gulp.task('get-deps', ['get-main-deps', 'get-component-deps']);
 
 gulp.task('jscs', function () {
   return gulp.src(['**/*.js', '!**/node_modules/**/*.js', '!www/**/*.js', '!_meta/**/*.js'])
@@ -40,7 +28,7 @@ gulp.task('jscs', function () {
     .pipe(jscs.reporter('fail'));
 });
 
-gulp.task('mocha', ['build'], function () {
+gulp.task('mocha', function () {
   return gulp.src('test/test.js', { read: false })
     .pipe(mocha());
 });
