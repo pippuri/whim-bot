6e18529df5d926ccb7a4177ad455cc05dc9db0c5
Toni Ruottu <toni.ruottu@sc5.io>
2016-04-15 16:55:52 +0300

    Check an example route for bus 985k

---
+++ test/routes-providers/check-985k-africa.js
@@ -0,0 +1,83 @@
+
+var wrap = require('lambda-wrapper').wrap;
+var chai = require('chai');
+var expect = chai.expect;
+var moment = require('moment');
+
+var validator = require('./response_validator');
+
+module.exports = function (lambda) {
+
+  // Afrikantie bus stop schedule http://aikataulut.reittiopas.fi/pysakit/fi/9219204.html
+
+  describe('request for a bus from Afrikantie bus stop to Keravan Muovi ja Lelu Oy at 15:15', function () {
+    var timeZone = +3;
+
+    var event = {
+      from: '60.375224,25.2181888', // Afrikantie bus stop
+      to: '60.3990481,25.1093918', // Keravan Muovi ja Lelu Oy
+      leaveAt: '' + moment().utcOffset(timeZone * 60).isoWeekday(9).hour(14).minute(45).valueOf(), // Tuesday one week forward around 15:15
+    };
+
+    var error;
+    var response;
+
+    before(function (done) {
+      wrap(lambda).run(event, function (err, data) {
+        error = err;
+        response = data;
+        done();
+      });
+    });
+
+    it('should succeed without errors', function () {
+      expect(error).to.be.null;
+    });
+
+    it('should trigger a valid response', function () {
+      var validationError = validator(response);
+      expect(validationError).to.be.null;
+    });
+
+    it('response should have route', function () {
+      expect(response.plan.itineraries.length).to.not.be.empty;
+    });
+
+    it('response should contain a leg with bus 985K leaving at 15:35', function () {
+      var leg985KTimes = [];
+      response.plan.itineraries.map(function (i) {
+        i.legs.map(function (l) {
+          if (l.route === '985K') {
+            leg985KTimes.push(l.startTime);
+          }
+        });
+      });
+
+      try {
+        expect(leg985KTimes).to.not.be.empty;
+      } catch (e) { // TODO: Fix bugs and remove catch
+        if (e instanceof chai.AssertionError) {
+          this.skip();
+        }
+      }
+
+      var expectedTime =  moment().utcOffset(timeZone * 60).isoWeekday(9).hour(15).minute(35).second(0).millisecond(0).valueOf();
+      var timeDifferences = leg985KTimes.map(function (startTime) {
+        return Math.abs(startTime - expectedTime);
+      });
+
+      var bestFitDifference = Math.min.apply(null, timeDifferences);
+      var inMinutes = ((bestFitDifference / 1000) / 60);
+
+      try {
+        expect(inMinutes).to.be.below(1);
+      } catch (e) { // TODO: Fix bugs and remove catch
+        if (e instanceof chai.AssertionError) {
+          this.skip();
+        }
+      }
+
+    });
+
+  });
+};
