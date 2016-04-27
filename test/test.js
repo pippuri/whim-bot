f1af19d9ab0356c2e6b045914adf87c079adc02c
Toni Ruottu <toni.ruottu@sc5.io>
2016-04-27 15:04:43 +0300

    Load environment variables before running tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,17 @@
+try {
+  var variables = require('../_meta/variables/s-variables-dev.json');
+  for (var key in variables) {
+    if (variables.hasOwnProperty(key)) {
+      var value = variables[key];
+      process.env[key] = value;
+    }
+
+  }
+
+} catch (e) {
+  // the json file probably does not exist
+}
+
 describe('MaaS.fi backend', function () {
   require('./api/test.js');
   require('./routes-providers/test.js');
