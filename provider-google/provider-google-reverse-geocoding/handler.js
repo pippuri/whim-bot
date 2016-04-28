e47c6985d2a24ee3fb1a3dfa278cbab111d7df34
mashahwan <moh.a.shahwan@gmail.com>
2016-04-28 18:29:16 +0300

    Google Reverse geocoding

---
+++ provider-google/provider-google-reverse-geocoding/handler.js
@@ -0,0 +1,9 @@
+// Require Logic
+var lib = require('./index');
+
+// Lambda Handler
+module.exports.handler = function (event, context) {
+  lib.respond(event, function (error, response) {
+    return context.done(error, response);
+  });
+};
