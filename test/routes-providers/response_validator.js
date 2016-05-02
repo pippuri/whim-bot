d53ab66d2c27f8ec0fa2ea132a7d5800238d56e2
Toni Ruottu <toni.ruottu@sc5.io>
2016-05-02 18:01:42 +0300

    Turn on ajv verbose option

--- test/routes-providers/response_validator.js
+++ test/routes-providers/response_validator.js
@@ -3,7 +3,7 @@ var ajvFactory = require('ajv');
 var schema = require('./response_schema.json');
 
 module.exports = function (response) {
-  var ajv = ajvFactory();
+  var ajv = ajvFactory({ verbose: true });
   var validate = ajv.compile(schema);
   var valid = validate(response);
   var validationError = valid ? null : JSON.stringify(validate.errors);
