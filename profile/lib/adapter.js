8da4512ff75e992e220c39b83fabfbc332ad892a
Hieu Nguyen <blackevil245@gmail.com>
2016-05-12 16:58:10 +0300

    Added profile-route-history + phone number check with Twilio

--- profile/lib/adapter.js
+++ profile/lib/adapter.js
@@ -1,39 +1,56 @@
+
 var AWS = require('aws-sdk');
 var Promise = require('bluebird');
+var LookupsClient = require('twilio').LookupsClient;
 
+var client = new LookupsClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_ACCOUNT_TOKEN);
 var cognitoIdentity = new AWS.CognitoIdentity({ region: process.env.AWS_REGION });
 
 Promise.promisifyAll(cognitoIdentity);
+// Promise.promisifyAll(client);
 
 /**
  * Check phoneNumber validity
- * TODO check for phoneNumber format
  */
 function checkPhoneNumber(phoneNumber) {
-  return true;
+  var promise = new Promise(function (resolve, reject) {
+    client.phoneNumbers(phoneNumber).get(function (error, number) {
+      if (error) {
+        reject(new Error('Wrong number'));
+      } else {
+        resolve(number);
+      }
+    });
+  });
+
+  return promise;
 }
 
 /**
  * Get/Create Cognito IdentityId
  */
 function getCognitoDeveloperIdentity(phoneNumber) {
-  var logins = {};
-  logins[process.env.COGNITO_DEVELOPER_PROVIDER] = 'tel:' + phoneNumber;
-  var options = {
-    IdentityPoolId: process.env.COGNITO_POOL_ID,
-    Logins: logins,
-  };
+  return checkPhoneNumber(phoneNumber)
+    .then((response) => {
+      var logins = {};
+      logins[process.env.COGNITO_DEVELOPER_PROVIDER] = 'tel:' + phoneNumber;
+      var options = {
+        IdentityPoolId: process.env.COGNITO_POOL_ID,
+        Logins: logins,
+      };
 
-  console.log('Getting cognito developer identity with', JSON.stringify(options, null, 2));
+      console.log('Getting cognito developer identity with', JSON.stringify(options, null, 2));
 
-  return cognitoIdentity.getOpenIdTokenForDeveloperIdentityAsync(options)
+      return cognitoIdentity.getOpenIdTokenForDeveloperIdentityAsync(options);
+    })
     .then((response) => {
       return {
         identityId: response.IdentityId,
-        cognitoToken: response.Token,
       };
     });
 }
 
-module.exports.checkPhoneNumber = checkPhoneNumber;
-module.exports.getCognitoDeveloperIdentity = getCognitoDeveloperIdentity;
+module.exports = {
+  checkPhoneNumber: checkPhoneNumber,
+  getCognitoDeveloperIdentity: getCognitoDeveloperIdentity,
+};
---
+++ profile/profile-history-route/index.js
@@ -0,0 +1,44 @@
+var AWS = require('aws-sdk');
+var Promise = require('bluebird');
+var lib = require('../lib/adapter');
+var moment = require('moment');
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+Promise.promisifyAll(docClient);
+
+/**
+ * Save route and time start of route onto DyanomoDB
+ */
+function saveRoute(event) {
+  if (event.route === undefined) {
+    return Promise.reject(new Error('No input route'));
+  }
+
+  return lib.getCognitoDeveloperIdentity(event.countryPhoneCode + event.plainPhone)
+    .then((response) => {
+      var item = {
+        IdentityId: response.identityId,
+        TimeEpoch: moment().unix(),
+        route: event.route,
+      };
+      console.log(item);
+      var params = {
+        TableName: process.env.DYNAMO_USER_ROUTE_HISTORY,
+        Item: item,
+        ReturnValues: 'ALL_OLD',
+        ReturnConsumedCapacity: 'TOTAL',
+      };
+      return docClient.putAsync(params);
+    });
+}
+
+module.exports.respond = function (event, callback) {
+  return saveRoute(event)
+    .then((response) => {
+      callback(null, response);
+    })
+    .catch((error) => {
+      callback(error);
+    });
+};
