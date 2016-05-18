b1768f4ba6538550341b29a35b8f3a225c67c50f
Hieu Nguyen <blackevil245@gmail.com>
2016-05-18 18:00:34 +0300

    Fix style error

--- store/store-packages/data.js
+++ store/store-packages/data.js
@@ -1,14 +1,2 @@
 module.exports = {
-  name: 'Pay as you go / Casual Commuter / Business Package / Ultimate mobility',
-  price: 300,
-  currency: 'euro',
-  formattedPrice: '300 \u20AC',
-  grantPoint: 1000,
-  features: [ // Unlimited only
-    'hsl', // -> icon + desc
-    'rental', // > business package
-
-  ],
-  provider: ['taxi', 'uber', 'hsl', 'private-chopper'],
-
 };
