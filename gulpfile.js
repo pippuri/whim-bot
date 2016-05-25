ad5b5362a666f058bf85c6409f79cff88eae26d4
Toni Ruottu <toni.ruottu@sc5.io>
2016-05-25 13:13:22 +0300

    Add validation for json config files with comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 
 const gulp = require('gulp');
 const jsonlint = require('gulp-jsonlint');
+const jsonclint = require('gulp-json-lint');
 const jshint = require('gulp-jshint');
 const jscs = require('gulp-jscs');
 const gmocha = require('gulp-mocha');
 const gulpSequence = require('gulp-sequence');
 
+const jsoncFiles = ['.jshintrc', '.jscsrc']; // json with comments
 const jsonFiles = ['**/*.json', '!**/node_modules/**/*.json', '!www/**/*.json', '!_meta/**/*.json'];
 const jsFiles = ['**/*.js', '!**/node_modules/**/*.js', '!www/**/*.js', '!_meta/**/*.js'];
+
+gulp.task('jsonclint', () => {
+
+  // Unfortunately does not support failOnError at the moment
+  // See https://github.com/panuhorsmalahti/gulp-json-lint/issues/2
+
+  return gulp.src(jsoncFiles)
+    .pipe(jsonclint({ comments: true }))
+    .pipe(jsonclint.report('verbose'));
+});
 
 gulp.task('jsonlint', () => {
   return gulp.src(jsonFiles)
@@ -35,7 +47,7 @@ gulp.task('mocha', () => {
     .pipe(gmocha());
 });
 
-gulp.task('validate', ['jsonlint', 'jshint', 'jscs']);
+gulp.task('validate', ['jsonclint', 'jsonlint', 'jshint', 'jscs']);
 
 gulp.task('test', gulpSequence('validate', 'mocha'));
 
