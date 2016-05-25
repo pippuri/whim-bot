4c2c5c76162f840d6f5bfdd586218926714ae49b
Hieu Nguyen <blackevil245@gmail.com>
2016-05-25 16:11:52 +0300

    Add store-single-package GET endpoint

---
+++ store/store-single-package/index.js
@@ -0,0 +1,23 @@
+// Library
+var SubscriptionMgr = require('../../lib/subscription-manager');
+
+function getSingleProduct(event) {
+  if (event.type === 'plan') {
+    return SubscriptionMgr.getPlanById(event.id);
+  } else if (event.type === 'addon') {
+    return SubscriptionMgr.getAddonById(event.id);
+  } else {
+    return Promise.reject(new TypeError('Invalid product type'));
+  }
+}
+
+module.exports.respond = function (event, callback) {
+  getSingleProduct(event)
+    .then(function (response) {
+      callback(null, response);
+    })
+    .catch(function (error) {
+      console.log(error.message);
+      callback(error);
+    });
+};
