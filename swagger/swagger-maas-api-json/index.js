e2ea45190f3d256dc8b557cf2536a28862e7e7bc
Hieu Nguyen <blackevil245@gmail.com>
2016-06-01 11:23:01 +0300

    Add swagger export setting to POST /profile/favorite-locations

--- swagger/swagger-maas-api-json/index.js
+++ swagger/swagger-maas-api-json/index.js
@@ -1,7 +1,7 @@
 var Promise = require('bluebird');
 var fs = require('fs');
 var path = require('path');
-var swaggerJson = JSON.parse(fs.readFileSync(path.join(__dirname, 'maas-api.json')));
+var swaggerJson = require('./maas-api.json');
 
 // Respond with Swagger API JSON
 function getApiJson() {
