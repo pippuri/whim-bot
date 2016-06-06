00edd9688a52361dc34ef7dcc3854ec04f40dbe7
Hieu Nguyen <blackevil245@gmail.com>
2016-06-06 10:50:32 +0300

    Add  support for s-function swaggerExport

--- swagger/swagger-maas-api-json/index.js
+++ swagger/swagger-maas-api-json/index.js
@@ -1,20 +1,17 @@
-var Promise = require('bluebird');
-var fs = require('fs');
-var path = require('path');
-var swaggerJson = require('./maas-api.json');
+const Promise = require('bluebird');
+const swaggerJson = require('./maas-api.json');
 
-// Respond with Swagger API JSON
-function getApiJson() {
+function derefJsonSchema() {
   return Promise.resolve(swaggerJson);
 }
 
 module.exports.respond = function (event, callback) {
-  getApiJson()
-  .then(function (response) {
-    callback(null, response);
-  })
-  .catch(function (err) {
-    console.log('This event caused error: ' + JSON.stringify(event, null, 2));
-    callback(err);
-  });
+  return derefJsonSchema()
+    .then(function (response) {
+      callback(null, response);
+    })
+    .catch(function (err) {
+      console.log('This event caused error: ' + JSON.stringify(event, null, 2));
+      callback(err);
+    });
 };
---
+++ swagger/swagger-populate/index.js
@@ -0,0 +1,37 @@
+const Promise = require('bluebird');
+const fs = require('fs');
+const path = require('path');
+const deref = require('json-schema-deref');
+const swaggerJson = require('../swagger-maas-api-json/maas-api.json');
+
+const derefPromise = Promise.promisify(deref);
+const writeFileAsync = Promise.promisify(fs.writeFile);
+
+function derefJsonSchema() {
+  return derefPromise(swaggerJson)
+    .then((fullSchema, error) => {
+      if (error !== undefined) {
+        return Promise.reject(error);
+      }
+
+      console.log(fullSchema.paths['/profile/favorite-locations'].post.parameters[0].schema);
+      return writeFileAsync(path.join(__dirname, '..', 'swagger-maas-api-json/maas-api.json'), JSON.stringify(fullSchema, null, 2));
+    })
+    .then(writefileError => {
+      if (writefileError) {
+        return Promise.reject(writefileError);
+      }
+
+      return Promise.resolve('Success');
+    });
+}
+
+module.exports.respond = (event, callback) => {
+  return derefJsonSchema()
+    .then(response => {
+      callback(null, response);
+    })
+    .catch(error => {
+      callback(error);
+    });
+};
