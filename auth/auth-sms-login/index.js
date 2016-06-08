2a618774589a3845e2c0b74ee64e9aebc6c607c3
Lauri Svan <lauri.svan@sc5.io>
2016-06-08 10:12:37 +0300

    Fix login to work on existing profiles

--- auth/auth-sms-login/index.js
+++ auth/auth-sms-login/index.js
@@ -8,12 +8,10 @@ var bus = require('../../lib/service-bus/index');
 var cognitoIdentity = new AWS.CognitoIdentity({ region: process.env.AWS_REGION });
 var cognitoSync = new AWS.CognitoSync({ region: process.env.AWS_REGION });
 var iot = new AWS.Iot({ region: process.env.AWS_REGION });
-var lambda = new AWS.Lambda({ region: process.env.AWS_REGION });
 
 Promise.promisifyAll(cognitoIdentity);
 Promise.promisifyAll(cognitoSync);
 Promise.promisifyAll(iot);
-Promise.promisifyAll(lambda, { suffix: 'Promise' });
 
 /**
  * Create or retrieve Amazon Cognito identity.
@@ -201,13 +199,10 @@ function smsLogin(phone, code) {
   .then(function () {
     return createUserThing(identityId, plainPhone, isSimulationUser);
   })
-  .then(function () {
+  .then(() => {
 
-    // Create a signed JSON web token
-    var token = jwt.sign({
-      id: identityId,
-    }, process.env.JWT_SECRET);
-
+  })
+  .then(() => {
     var profilePayload = {
       identityId: identityId,
       payload: {
@@ -215,20 +210,24 @@ function smsLogin(phone, code) {
       },
     };
 
-    // Run profile-create which create a new user profile if not exist
-    return bus.call('MaaS-profile-create', profilePayload)
-    .then(response => {
-      var message = (!response.hasOwnProperty('errorMessage') && !response.hasOwnProperty('errorType')) ? 'created' : 'abort:existed';
-      return {
-        id_token: token,
-        cognito_id: identityId,
-        cognito_token: cognitoToken,
-        cognito_pool: process.env.COGNITO_POOL_ID,
-        cognito_provider: 'cognito-identity.amazonaws.com',
-        profile_create_message: message,
-      };
-    });
+    // First try to fetch an existing identity
+    return bus.call('MaaS-profile-info', { identityId: identityId })
+      .catch(error => {
+        // No profile found
+        return bus.call('MaaS-profile-create', profilePayload);
+      });
+  })
+  .then(_empty => {
+    // Create a signed JSON web token
+    const token = jwt.sign({ id: identityId }, process.env.JWT_SECRET);
 
+    return {
+      id_token: token,
+      cognito_id: identityId,
+      cognito_token: cognitoToken,
+      cognito_pool: process.env.COGNITO_POOL_ID,
+      cognito_provider: 'cognito-identity.amazonaws.com',
+    };
   });
 }
 
