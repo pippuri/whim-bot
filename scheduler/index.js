211ba6dbc531d57a073158b009bb313f045fc7a7
mashahwan <moh.a.shahwan@gmail.com>
2016-06-08 15:13:24 +0300

    resolve conflict

--- scheduler/index.js
+++ scheduler/index.js
@@ -4,7 +4,7 @@ var lib = require('../lib/utilities/index');
 var bus = require('../lib/service-bus/index');
 var _ = require('lodash');
 var aws = require('aws-sdk');
-var lambda = new aws.Lambda();
+
 var docClient = new aws.DynamoDB.DocumentClient();
 
 function updateFunctionStatus(functionId, status, FnResponse) {
@@ -14,80 +14,86 @@ function updateFunctionStatus(functionId, status, FnResponse) {
     return Promise.reject(new Error('Missing functionId'));
   }
 
-  return lib.documentExist('MaaS-scheduled-functions-dev', 'functionId', functionId, null, null)
-    .then((response) => {
-      if (response === true) { // True if existed
-        var params = {
-          TableName: 'MaaS-scheduled-functions-dev',
-          Key: {
-            functionId: functionId,
-          },
-          UpdateExpression: 'SET flag = :s, functionResponse = :FunctionResponse',
-          ExpressionAttributeValues: {
-            ':s': status,
-            ':FunctionResponse': FnResponse,
-          },
-          ReturnValues: 'UPDATED_NEW',
-          ReturnConsumedCapacity: 'INDEXES',
-        };
-
-        return bus.call('Dynamo-update', params);
-      } else {
+  return lib.documentExist(process.env.MAAS_SCHEDULER, 'functionId', functionId, null, null)
+    .then(response => {
+      if (response === false) { // False means not existed
         return Promise.reject(new Error('Function Not Existed'));
       }
+
+      var params = {
+        TableName: process.env.MAAS_SCHEDULER,
+        Key: {
+          functionId: functionId,
+        },
+        UpdateExpression: 'SET #flag = :s, #functionResponse = :functionResponse',
+        ExpressionAttributeValues: {
+          ':s': status,
+          ':functionResponse': Object.keys(FnResponse).length === 0 ? 'Error' : FnResponse,
+        },
+        ExpressionAttributeNames: {
+          '#flag': 'flag',
+          '#functionResponse': 'functionResponse',
+        },
+        ReturnValues: 'UPDATED_NEW',
+        ReturnConsumedCapacity: 'INDEXES',
+      };
+
+      return bus.call('Dynamo-update', params);
     });
 }
 
-function getScheduledFunction(event) {
+function getScheduledFunction() {
 
   var params = {
-    TableName: 'MaaS-scheduled-functions-dev',
+    TableName: process.env.MAAS_SCHEDULER,
+
     FilterExpression: 'flag = :Flag AND invokeTime <= :nvkTime',
     ExpressionAttributeValues: {
+
       ':Flag': 'SCHEDULED',
       ':nvkTime': Date.now(),
     },
   };
 
   return new Promise(function (resolve, reject) {
-    docClient.scan(params, function onScan(err, data) {
+
+    // TODO move this to service-bus
+    docClient.scan(params, (err, data) => {
       if (err) {
         reject(new Error('Unable to query. Error:' + JSON.stringify(err, null, 2)));
-      } else {
-        resolve(data);
       }
+
+      resolve(data);
     });
   });
+}
+
+function runScheduledFunction(event) {
+  return getScheduledFunction()
+    .then(response => {
+      response.Items.forEach(item => {
+
+        updateFunctionStatus(item.functionId, 'STARTED', response);
+        return bus.call(item.functionName, JSON.parse(item.parameters))
+          .then(response => {
+            return updateFunctionStatus(item.functionId, 'FINISHED', response);
+          })
+          .catch(error => {
+            return updateFunctionStatus(item.functionId, 'FAILED', error);
+          });
+      });
+    });
 }
 
 /**
  * Export respond to Handler
  */
-module.exports.respond = function (event, callback) {
-  return getScheduledFunction(event)
-    .then((response) => {
-      response.Items.forEach(function (item) {
-        var params = {
-          FunctionName: item.functionName,
-          Payload: item.parameters,
-        };
-        updateFunctionStatus(item.functionId, 'STARTED', null);
-        lambda.invoke(params, function (err, data) {
-          if (err) {
-            updateFunctionStatus(item.functionId, 'FAILED', data.Payload);
-            callback(err);
-          } else {
-            if (data.hasOwnProperty('FunctionError')) {
-              updateFunctionStatus(item.functionId, 'FAILED', data.Payload);
-            } else {
-              updateFunctionStatus(item.functionId, 'FINISHED', data.Payload);
-              callback(null, JSON.stringify(data, null, 0));
-            }
-          }
-        });
-      });
+module.exports.respond = (event, callback) => {
+  return runScheduledFunction(event)
+    .then(response => {
+      callback(null, response);
     })
-    .catch((error) => {
+    .catch(error => {
       callback(error);
     });
 };
