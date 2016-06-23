b563d01cffa7cad547cfc89199871fb5c0a4357d
Hieu Nguyen <blackevil245@gmail.com>
2016-06-23 12:59:45 +0300

    Add time proceduce and trigger for itinerary

--- scripts/migrations/initial.js
+++ scripts/migrations/initial.js
@@ -26,7 +26,24 @@ exports.up = function (knex) {
       table.jsonb('token');
       table.jsonb('terms');
       table.jsonb('meta');
+      table.timestamp('created').notNullable().defaultTo(knex.raw('now()'));
+      table.timestamp('modified');
     })
+    .raw(`
+      CREATE OR REPLACE FUNCTION proc_update_timestamp()
+      RETURNS trigger AS
+      $Booking$
+        BEGIN
+          NEW.modified = now();
+          RETURN NEW;
+        END;
+      $Booking$ LANGUAGE plpgsql;
+    `)
+    .raw(`
+      CREATE TRIGGER "trig_update_timestamp" AFTER UPDATE OR INSERT
+      ON "Booking"
+      FOR EACH ROW EXECUTE PROCEDURE proc_update_timestamp();
+    `)
     .createTable('Leg', table => {
       table.uuid('id').primary();
       table.uuid('itineraryId').references('Itinerary.id');
@@ -52,6 +69,9 @@ exports.up = function (knex) {
 
 exports.down = function (knex) {
   return knex.schema
+    .dropTableIfExists('Leg')
+    .dropTableIfExists('Itinerary')
+    .dropTableIfExists('Booking')
     .dropTableIfExists('Leg')
     .dropTableIfExists('Itinerary')
     .dropTableIfExists('Booking');
