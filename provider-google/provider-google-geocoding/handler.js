47f2967ddc63e806f854db2161c748e1040cda04
Lauri Svan <lauri.svan@sc5.io>
2016-06-26 13:27:38 +0300

    Update request-promise-lite; prune unnecessary deps

--- provider-google/provider-google-geocoding/handler.js
+++ provider-google/provider-google-geocoding/handler.js
@@ -1,16 +1,5 @@
 'use strict';
 
-/**
- * Serverless Module: Lambda Handler
- * - Your lambda functions should be a thin wrapper around your own separate
- * modules, to keep your code testable, reusable and AWS independent
- * - 'serverless-helpers-js' module is required for Serverless ENV var support.  Hopefully, AWS will add ENV support to Lambda soon :)
- */
-
-// Require Serverless ENV vars
-const ServerlessHelpers = require('serverless-helpers-js');
-ServerlessHelpers.loadEnv();
-
 // Require Logic
 const lib = require('./index');
 
