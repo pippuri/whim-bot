b9d4af32ac699f081cd38485f7f40504d2f9b02c
Lauri Svan <lauri.svan@sc5.io>
2016-06-28 17:39:23 +0300

    Fix: Itineraries with unknown agencies failed

--- test/api/itineraries-endpoint/itinerary-create.js
+++ test/api/itineraries-endpoint/itinerary-create.js
@@ -4,12 +4,13 @@ const wrap = require('lambda-wrapper').wrap;
 const expect = require('chai').expect;
 const validator = require('../../../lib/validator');
 const schema = require('../../../itineraries/itinerary-create/response-schema.json');
-const event = require('../../../itineraries/itinerary-create/event.json');
 const maasUtils = require('../../../lib/utils');
+const event = require('../../../itineraries/itinerary-create/event.json');
+const moraEvent = require('./itinerary-mora-orsa.json');
 
 module.exports = function (lambda) {
 
-  describe('create itinerary', () => {
+  describe('create itinerary with known agencies', () => {
     let error;
     let response;
 
@@ -40,7 +41,40 @@ module.exports = function (lambda) {
         .then(validationError => {
           expect(validationError).to.be.null;
         });
-    }
-    );
+    });
+  });
+
+  describe('create itinerary with unknown agencies', () => {
+    let error;
+    let response;
+
+    before(done => {
+      // Sign the event data (Travis seems to have problems repeating the signatures)
+      const newItinerary = Object.assign({}, moraEvent.itinerary);
+      delete newItinerary.signature;
+      moraEvent.itinerary.signature = maasUtils.sign(newItinerary, process.env.MAAS_SIGNING_SECRET);
+
+      wrap(lambda).run(moraEvent, (_error, _response) => {
+        error = _error;
+        response = _response;
+        done();
+      });
+    });
+
+    it('should succeed without errors', () => {
+      if (error) {
+        console.log(error);
+        console.log(error.stack);
+      }
+
+      expect(error).to.be.null;
+    });
+
+    it('should trigger a valid response', () => {
+      return validator.validate(response, schema)
+        .then(validationError => {
+          expect(validationError).to.be.null;
+        });
+    });
   });
 };
