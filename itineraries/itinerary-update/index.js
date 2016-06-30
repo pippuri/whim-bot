f26e9add2254b0902d2f6a84153f0c8a4676ea55
Hieu Nguyen <blackevil245@gmail.com>
2016-06-30 17:13:57 +0300

    Feature itinerary update

--- itineraries/itinerary-update/index.js
+++ itineraries/itinerary-update/index.js
@@ -1,29 +1,101 @@
 'use strict';
 
-// const Promise = require('bluebird');
-// const lib = require('../lib/index');
-// const knex = lib.initKnex();
-// const MaasError = require('../../lib/errors/MaaSError');
-// const _ = require('lodash');
+const Promise = require('bluebird');
+const allowedFields = require('../../lib/models/editableFields.json');
+const models = require('../../lib/models/index');
+const MaasError = require('../../lib/errors/MaaSError');
+const _ = require('lodash');
+const stateLib = require('../../lib/states/index');
+
+let knex;
+
+const allowedItineraryFields = allowedFields.Itinerary;
+const returnFields = ['id'];
 
 function updateItinerary(event) {
-  // const allowedFields = [];
 
-  if (!event.hasOwnProperty('identityId') || event.identityId !== '') {
-    return;
+  if (!event.hasOwnProperty('identityId') || event.identityId === '') {
+    return Promise.reject(new MaasError('Missing identityId input', 401));
   }
 
-  if (!event.hasOwnProperty('payload') || Object.keys('payload').length !== 0) {
-    return;
+  if (!event.hasOwnProperty('itineraryId') || !event.itineraryId.match(/[A-F0-9]{8}(-[A-F0-9]{4}){3}-[A-F0-9]{12}/g)) {
+    return Promise.reject(new MaasError('Missing or invalid itineraryId'));
   }
+
+  if (!event.hasOwnProperty('payload') || Object.keys('payload').length === 0) {
+    return Promise.reject(new MaasError('Payload empty. No update was processed'));
+  }
+
+  // Compare input key vs allowed keys
+  Object.keys(event.payload).map(key => { // eslint-disable-line consistent-return
+    if (!_.includes(allowedItineraryFields, key.toLowerCase())) {
+      return Promise.reject(new MaasError(`Request contains unallowed field(s), allow only [${allowedItineraryFields}]`));
+    }
+  });
+
+  // Add modified time
+  const modifiedTime = new Date().getTime();
+  event.payload.modified = new Date(modifiedTime).toISOString();
+
+  // Get old state
+  return knex.select('state')
+    .from('Itinerary')
+    .where('id', event.itineraryId)
+    .then(_oldState => {
+      const oldState = _oldState[0];
+      if (typeof oldState === typeof undefined) {
+        return Promise.reject(new MaasError(`No oldState found for itineraryId ${event.itineraryId}, server error`, 500));
+      }
+
+      // Double check if oldState on database is still valid
+      if (event.payload.state || !stateLib.validNewState('Itinerary', oldState, event.payload.state)) {
+        return Promise.reject('State unavailable');
+      }
+      const promiseQueue = [];
+
+      // Queue up state change process
+      if (event.payload.state) {
+        promiseQueue.push(stateLib.changeState('Itinerary', knex, event.itineraryId, _oldState, event.payload.state));
+        delete event.payload.state;
+      }
+
+      // Queue up other fields update
+      promiseQueue.push(
+        knex.update(event.payload, returnFields.concat(Object.keys(event.payload)))
+          .into('Itinerary')
+          .where('id', event.itineraryId)
+      );
+
+      // Process the queue
+      return Promise.all(promiseQueue);
+    })
+    .spread(response => {
+      // if (response.length === 0) {
+      //   return Promise.reject(new MaasError(`Itinerary id ${event.bookingId} not updated, server error`, 500));
+      // } else if (response.length !== 1) {
+      //   return Promise.reject(new MaasError(`Database returns more than 1 result with id ${event.bookingId}, server error`, 500));
+      // }
+      console.warn(response);
+      const booking = response[response.length - 1];
+      return booking;
+    });
 }
 
 module.exports.respond = (event, callback) => {
-  updateItinerary(event)
+  return models.init()
+    .then(_knex => {
+      knex = _knex;
+      return updateItinerary(event);
+    })
     .then(response => {
       callback(null, response);
     })
     .then(error => {
       callback(error);
+    })
+    .finally(() => {
+      if (knex) {
+        knex.destroy();
+      }
     });
 };
