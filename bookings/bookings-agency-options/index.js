6f3ce8d5174c0624428e67aca5382ecae2507a30
Hieu Nguyen <blackevil245@gmail.com>
2016-07-08 17:06:37 +0300

    Booking-agency-options now work with tsp Sixt

--- bookings/bookings-agency-options/index.js
+++ bookings/bookings-agency-options/index.js
@@ -3,7 +3,6 @@
 const request = require('request-promise-lite');
 const Promise = require('bluebird');
 const MaasError = require('../../lib/errors/MaaSError');
-const _ = require('lodash');
 const utils = require('../../lib/utils/index');
 const tsp = require('../../lib/tsp/index');
 
@@ -13,36 +12,40 @@ function getAgencyProductOptions(event) {
     return Promise.reject(new MaasError('Missing input agencyId', 400));
   }
 
-  if (!event.queryString.hasOwnProperty('startTime') || event.queryString.startTime === '') {
+  if (!event.payload.hasOwnProperty('startTime') || event.payload.startTime === '') {
     return Promise.reject(new MaasError('startTime querystring empty or missing'));
   }
 
-  if (!event.queryString.hasOwnProperty('from') || event.queryString.from === '') {
+  if (!event.payload.hasOwnProperty('from') || event.payload.from === '') {
     return Promise.reject(new MaasError('from querystring empty or missing'));
   }
 
   let queryString = '/?';
-  const queryStringKeys = Object.keys(event.queryString);
+  const queryStringKeys = Object.keys(event.payload);
 
   queryStringKeys.map(queryKey => {
-    queryString += queryKey + '=' + event.queryString[queryKey];
+    queryString += queryKey + '=' + event.payload[queryKey];
     if (queryStringKeys.indexOf(queryKey) < queryStringKeys.length - 1) {
       queryString += '&';
     }
   });
 
   return tsp.findAgency(event.agencyId)
-    .then(tsp => request.get(tsp.adapter.baseUrl + tsp.adapter.endpoints.get.options + queryString))
-    .then(options => {
-      if (!_.isArray(options)) {
-        options = [options];
+    .then(tsp => request.get(tsp.adapter.baseUrl + tsp.adapter.endpoints.get.options + queryString, { json: true }))
+    .then(response => {
+      // If response.options is undefined, return empty
+      if (typeof response.options === typeof undefined) {
+        return {
+          options: [],
+          meta: {},
+        };
       }
-
-      options.forEach(option => {
-        if (typeof option === typeof {}) {
+      response.options.forEach(option => {
+        if (typeof option === 'object') {
           option.signature = utils.sign(option, process.env.MAAS_SIGNING_SECRET);
         }
       });
+      return response;
     });
 }
 
