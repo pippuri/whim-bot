9b69d3a2a7b731022f63a3beed0da0a2ea3aea7a
Hieu Nguyen <blackevil245@gmail.com>
2016-07-18 17:27:43 +0300

    Create dummy booking if not exist

--- test/api/bookings-endpoint/feature-bookings-retrieve.js
+++ test/api/bookings-endpoint/feature-bookings-retrieve.js
@@ -4,9 +4,14 @@ const expect = require('chai').expect;
 const wrap = require('lambda-wrapper').wrap;
 const validator = require('../../../lib/validator/index');
 const schema = require('../../../bookings/bookings-retrieve/response-schema.json');
-const event = require('../../../bookings/bookings-retrieve/event.json');
+const bus = require('../../../lib/service-bus/index');
 
 module.exports = function (lambda) {
+
+  const event = {
+    identityId: 'eu-west-1: 00000000-cafe-cafe-cafe-000000000000',
+    bookingId: 'BCB5C1D1-43A2-11E6-A001-2D65C8A4E851',
+  };
 
   describe('retrieve an existing booking', () => {
 
@@ -15,9 +20,25 @@ module.exports = function (lambda) {
 
     before(done => {
       wrap(lambda).run(event, (err, res) => {
-        response = res;
-        error = err;
-        done();
+        if (!err) {
+          response = res;
+          error = err;
+          done();
+        } else {
+          // Try to create a dummy booking if somehow the dummy wasn't already there
+          bus.call('MaaS-bookings-create', require('../../../bookings/bookings-create/event.json'))
+            .then(response => {
+              const newEvent = {
+                identityId: response.customer.id ? response.customer.id : 'eu-west-1:00000000-cafe-cafe-cafe-000000000000',
+                bookingId: response.id,
+              };
+              wrap(lambda).run(newEvent, (err, res) => {
+                response = res;
+                error = err;
+                done();
+              });
+            });
+        }
       });
     });
 
