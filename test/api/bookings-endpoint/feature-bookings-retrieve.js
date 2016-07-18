dfb20c2156cc08ebbd7cda35c0edc0addcc44e6e
Hieu Nguyen <blackevil245@gmail.com>
2016-07-18 14:50:31 +0300

    Added test for bookings-retrive + some req/res schemas

---
+++ test/api/bookings-endpoint/feature-bookings-retrieve.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const expect = require('chai').expect;
+const wrap = require('lambda-wrapper').wrap;
+const validator = require('../../../lib/validator/index');
+const schema = require('../../../bookings/bookings-retrieve/response-schema.json');
+const event = require('../../../bookings/bookings-retrieve/event.json');
+
+module.exports = function (lambda) {
+
+  describe('retrieve an existing booking', () => {
+
+    let response;
+    let error;
+
+    before(done => {
+      wrap(lambda).run(event, (err, res) => {
+        response = res;
+        error = err;
+        done();
+      });
+    });
+
+    it('should return a valid response', () => {
+      return validator.validate(response, schema)
+        .then(validationError => {
+          expect(validationError).to.be.null;
+        });
+    });
+
+    it('should succeed without error', () => {
+      expect(error).to.be.null;
+    });
+  });
+};
--- test/api/bookings-endpoint/index.js
+++ test/api/bookings-endpoint/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const agencyOptionsTest = require('./feature-agency-options.js');
+const bookingsRetrieveTest = require('./feature-bookings-retrieve');
 
 describe('bookings endpoint', function () {
   this.timeout(20000);
 
-  const lambda = require('../../../bookings/bookings-agency-options/handler.js');
-  agencyOptionsTest(lambda);
+  agencyOptionsTest(require('../../../bookings/bookings-agency-options/handler.js'));
+  bookingsRetrieveTest(require('../../../bookings/bookings-retrieve/handler.js'));
 });
