2f9b9cd67afecdd26936d17d825b7b68b6d79693
Sami Pippuri <pippuri@gmail.com>
2016-07-20 18:40:25 +0300

    webhook handles also state changes in Chargebee now

--- profile/profile-webhook/chargebee.js
+++ profile/profile-webhook/chargebee.js
@@ -2,8 +2,12 @@
 
 const Promise = require('bluebird');
 const Subscription = require('../../lib/subscription-manager/index.js');
+const utils = require('../../lib/utils/index.js');
 const MaaS = require('../../lib/maas-operation/index.js');
 const PROFILE_FIELD = 'profile';
+const BALANCE_FIELD = 'balance';
+const PLAN_FIELD = 'plans';
+const WHIM_DEFAULT = process.env.DEFAULT_WHIM_PLAN;
 
 /*
  Called when subscription gets renewed, activated etc
@@ -12,7 +16,27 @@ const PROFILE_FIELD = 'profile';
 function handleSubscriptionUpdate(event, payload) {
   const profile = Subscription.formatUser(payload.content);
   const identity = profile.identityId;
-  return MaaS.updateCustomerProfile(identity, PROFILE_FIELD, profile);
+  const activePlan = profile.plan;
+  
+  return Subscription.getPlans().then( plans => {
+    if (!plans || Object.keys(plans).length < 1 || !plans.hasOwnProperty('list')) {
+       return Promise.reject(new Error('Communication Error with Chargebee plans'));
+    }
+
+    for (var i=0; i < plans.list.length; i++ ) {
+      const plan = plans.list[i];
+      // reformat to standard from chargebee
+      const userPlan = utils.parseSingleChargebeePlan(plan);
+      if (userPlan && (userPlan.id === activePlan.id)) {
+        // update the user profile with the points of this renewal
+        return Promise.all([ 
+          MaaS.updateCustomerProfile(identity, PLAN_FIELD, [userPlan]), 
+          MaaS.updateCustomerProfile(identity, BALANCE_FIELD, userPlan.pointGrant) 
+        ]);
+      }
+    }
+    return Promise.reject(new Error('Did not find the active plan'));
+  });
 }
 
 /**
@@ -31,7 +55,13 @@ function handleDetailsUpdate(event, payload) {
 function handleCancellation(event, payload) {
   const profile = Subscription.formatUser(payload.content);
   const identity = profile.identityId;
-  return MaaS.updateCustomerProfile(identity, PROFILE_FIELD, profile);
+  return Subscription.getPlanById(WHIM_DEFAULT)
+    .then( plan => {
+       return MaaS.updateCustomerProfile(identity, PLAN_FIELD, [plan.plan]);
+    })
+    .then( () => {
+      return MaaS.updateCustomerProfile(identity, BALANCE_FIELD, 0);
+    });
 }
 
 function handleWebhook(event) {
@@ -62,13 +92,13 @@ function handleWebhook(event) {
     case 'card_updated':
     case 'payment_succeeded':
     case 'card_added':
+    case 'payment_failed':
+    case 'card_deleted':
+    case 'card_expired':
     case 'card_expiry_reminder':
       return handleDetailsUpdate(event, payload);
-    case 'payment_failed':
     case 'customer_deleted':
-    case 'card_expired':
     case 'subscription_cancelled':
-    case 'card_deleted':
     case 'subscription_deleted':
       return handleCancellation(event, payload);
     default:
