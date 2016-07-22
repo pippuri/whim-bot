6ecb2a43c98b71db5171f8c6473475995a59da19
Sami Pippuri <pippuri@gmail.com>
2016-07-22 14:59:32 +0300

    plan upgrades and downgrdes

--- profile/profile-active-plan-put/index.js
+++ profile/profile-active-plan-put/index.js
@@ -5,9 +5,9 @@ const lib = require('../../lib/utils/index');
 const bus = require('../../lib/service-bus/index');
 
 function setActivePlan(event) {
+  let oldLevel;
   let oldBalance;
   let newPlan;
-
   if (Object.keys(event).length === 0) {
     return Promise.reject(new Error('400: Input missing'));
   }
@@ -29,11 +29,12 @@ function setActivePlan(event) {
 
       return bus.call('MaaS-profile-info', { // Then get user balance
         identityId: event.identityId,
-        attributes: 'balance',
+        attributes: 'balance,planlevel',
       });
     })
     .then(response => { // Then retrieve plan information
       oldBalance = response.Item.balance;
+      oldLevel = response.Item.planlevel ? response.Item.planlevel : 0;
       return bus.call('MaaS-store-single-package', {
         id: event.planId,
         type: 'plan',
@@ -62,10 +63,30 @@ function setActivePlan(event) {
     .then(response => { // Then set the new point balance
       console.log('New pointGrant: ', newPlan.pointGrant);
       console.log('Old balance: ', oldBalance);
+      console.log('Old level: ', oldLevel);
+      console.log('New level: ', newPlan.level);
+      let newBalance = oldBalance;
+      if (newPlan.level > 0 && (newPlan.level > oldLevel)) {
+        // handle upgrade
+        const slices = newPlan.tiers.slice(oldLevel, newPlan.level); // slice the array with tiers from zero
+        for (const level of slices) {
+          newBalance += level;
+        }
+        console.log('New tier grant', newBalance);
+      } else if (newPlan.level === oldLevel) {
+        return Promise.resolve( { message: 'Old plan already the same level' } );
+      } else {
+        // this was a downgrade
+        if (newBalance > newPlan.pointGrant) {
+          newBalance = newPlan.pointGrant;
+        }
+        console.log('Downgrade: points', newBalance);
+      }
       const params2 = {
         identityId: event.identityId,
         payload: {
-          balance: newPlan.pointGrant + 0.2 * oldBalance,
+          balance: newBalance,
+          planlevel: newPlan.level,
         },
       };
       console.log(params2);
@@ -89,7 +110,7 @@ module.exports.respond = (event, callback) => {
       callback(null, response);
     })
     .catch(error => {
-      console.log('This event caused error: ' + JSON.stringify(event, null, 2));
+      console.log('This caused error: ' + JSON.stringify(error, null, 2));
       callback(error);
     });
 };
