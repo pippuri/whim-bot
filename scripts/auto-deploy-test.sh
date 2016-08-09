f670920a05371eaaf95a1f04b4bd17af42fabe0d
Hieu Nguyen <blackevil245@gmail.com>
2016-08-09 15:41:32 +0300

    Fix travis continuous deployment

--- scripts/auto-deploy-test.sh
+++ scripts/auto-deploy-test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# Deploy MaaS Custom Authorizer
-# Kenneth Falck <kennu@sc5.io> 2016
+# James Nguyen
 
-export AWS_PROFILE="maas"
+# Try to deploy authorizer, not fail even on error
+set +e
 export API_ID="9hmh5en1ch"
 export AUTHORIZER_ID="r94b5z"
 export AWS_REGION="eu-west-1"
@@ -10,14 +10,18 @@ export AWS_ACCOUNT="756207178743"
 export STAGE="test"
 export LAMBDA="MaaS-auth-custom-authorizer"
 
-# First we update the authorizer to call the right lambda function including the qualifier :xxx at the end of the function ARN
+## First we update the authorizer to call the right lambda function including the qualifier :xxx at the end of the function ARN
 aws apigateway update-authorizer --rest-api-id $API_ID --authorizer-id $AUTHORIZER_ID --patch-operations op=replace,path=/authorizerUri,value=arn:aws:apigateway:$AWS_REGION:lambda:path/2015-03-31/functions/arn:aws:lambda:$AWS_REGION:$AWS_ACCOUNT:function:$LAMBDA:$STAGE/invocations
 
-# Then we give API Gateway permission to invoke the authorizer using resource policies on the Lambda function
+## Then we give API Gateway permission to invoke the authorizer using resource policies on the Lambda function
 aws lambda add-permission --function-name $LAMBDA --statement-id Stmt1 --action lambda:InvokeFunction --principal apigateway.amazonaws.com --qualifier $STAGE --source-arn arn:aws:execute-api:$AWS_REGION:$AWS_ACCOUNT:$API_ID/authorizers/$AUTHORIZER_ID
 
-# You can check the structure of your authorizer with
+## You can check the structure of your authorizer with
 aws apigateway get-authorizer --rest-api-id $API_ID --authorizer-id $AUTHORIZER_ID
 
-# You can also check the policy against the lambda function with
+## You can also check the policy against the lambda function with
 aws lambda get-policy --function-name $LAMBDA --qualifier $STAGE
+set -e
+
+# Deploy all endpoint + lambda to test stage on every master merge
+npm run deploy-test:all
