5eb6ebf84d9e47e4eec8a650368d16cb3d95064e
Lauri Svan <lauri.svan@iki.fi>
2016-08-19 13:54:08 +0300

    Separate itineraries query into a separate endpoint

---
+++ itineraries/itinerary-list/index.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const Promise = require('bluebird');
+const MaaSError = require('../../lib/errors/MaaSError');
+const utils = require('../../lib/utils');
+const models = require('../../lib/models');
+const stateMachine = require('../../lib/states/index').StateMachine;
+const Database = models.Database;
+
+/**
+ * Validates the input event to have identityId and itineraryId
+ * (passed as query string).
+ *
+ * @param {object} event the input event, as received from API Gateway
+ */
+function parseAndValidateInput(event) {
+  const identityId = event.identityId;
+  const startTime = (event.startTime === '') ? 0 : parseInt(event.startTime, 10);
+  const endTime = (event.endTime === '') ? 0 : parseInt(event.endTime, 10);
+  const states = event.states.split(',');
+  const validStates = stateMachine.getAllStates('Itinerary');
+
+  if (typeof identityId === 'undefined' || identityId === '') {
+    return Promise.reject(new MaaSError('Missing identityId input', 401));
+  }
+
+  if (startTime <= 0) {
+    const message = `Invalid startTime: ${event.startTime}; expecting positive integer`;
+    return Promise.reject(new MaaSError(message, 400));
+  }
+
+  if (endTime <= 0) {
+    const message = `Invalid startTime: ${event.endTime}; expecting positive integer`;
+    return Promise.reject(new MaaSError(message, 400));
+  }
+
+  if (startTime < endTime) {
+    const message = `Invalid endTime: ${event.endTime}; must be greater than equal to startTime ${event.startTime}`;
+    return Promise.reject(new MaaSError(message, 400));
+  }
+
+  for (let i = 0; i < states.length; i++) {
+    const state = states[i];
+    if (!validStates.includes(state)) {
+      const message = `Invalid state ${state} instates, should be one of ${states}`;
+      return Promise.reject(new MaaSError(message, 400));
+    }
+  }
+
+  return Promise.resolve({
+    identityId: identityId,
+    startTime: startTime,
+    endTime: endTime,
+    states: states,
+  });
+}
+
+function fetchItineraries(identityId, startTime, endTime, states) {
+  // Get the old state
+  return models.Itinerary.query()
+    .findById(itineraryId)
+    .eager('[legs, legs.booking]')
+    .then(itinerary => {
+      // Handle not found
+      if (typeof itinerary === typeof undefined) {
+        return Promise.reject(new MaaSError(`No item found with itineraryId ${itineraryId}`, 404));
+      }
+
+      // Handle item not user's itinerary
+      if (itinerary.identityId !== identityId) {
+        return Promise.reject(new MaaSError(`Itinerary ${itineraryId} not owned by the user`, 403));
+      }
+
+      return Promise.resolve(itinerary);
+    });
+}
+
+function formatResponse(itineraries) {
+  const filtered = itineraries.map(utils.removeNulls);
+
+  return Promise.resolve({
+    itineraries: filtered,
+    maas: {},
+  });
+}
+
+module.exports.respond = (event, callback) => {
+
+  return Promise.all([
+    Database.init(),
+    parseAndValidateInput(event),
+  ])
+    .spread((knex, parsed) => fetchItineraries(parsed.identityId, parsed.startTime, parsed.endTime, parsed.states))
+    .then(itineraries => formatResponse(itineraries))
+    .then(response => {
+      Database.cleanup()
+        .then(() => callback(null, response));
+    })
+    .catch(_error => {
+      console.warn(`Caught an error:  ${_error.message}, ${JSON.stringify(_error, null, 2)}`);
+      console.warn('This event caused error: ' + JSON.stringify(event, null, 2));
+      console.warn(_error.stack);
+
+      // Uncaught, unexpected error
+      Database.cleanup()
+      .then(() => {
+        if (_error instanceof MaaSError) {
+          callback(_error);
+          return;
+        }
+
+        callback(new MaaSError(`Internal server error: ${_error.toString()}`, 500));
+      });
+    });
+};
