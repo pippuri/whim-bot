644ee91033ab5d411282b579d7542829b7413f5c
Lauri Svan <lauri.svan@iki.fi>
2016-09-01 11:23:30 +0300

    Add istanbul code coverage reporting

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const eslint = require('gulp-eslint');
 const gmocha = require('gulp-mocha');
 const gulpSequence = require('gulp-sequence');
 const gutil = require('gulp-util');
+const istanbul = require('gulp-istanbul');
 
 const jsoncFiles = ['.eslintrc']; // json with comments
 const jsonFiles = ['**/*.json', '!**/node_modules/**/*.json', '!www/**/*.json', '!_meta/**/*.json'];
@@ -37,9 +38,17 @@ gulp.task('eslint', () => {
 });
 
 gulp.task('mocha', () => {
-  return gulp.src('test/test.js', { read: false })
-    .pipe(gmocha())
-    .on('error', gutil.log);
+  return gulp.src(jsFiles)
+  .pipe(istanbul())
+  .pipe(istanbul.hookRequire())
+  .on('finish', () => {
+    return gulp.src('test/test.js', { read: false })
+      .pipe(gmocha())
+      .pipe(istanbul.writeReports())
+      // Skip thresholds until we have more coverage
+      //.pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
+      .on('error', gutil.log);
+  });
 });
 
 gulp.task('validate', ['jsonclint', 'jsonlint', 'eslint']);
