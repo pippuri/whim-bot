7485b645a41cb5074543c12b3a51be7596899c99
Hieu Nguyen <Blackevil245@gmail.com>
2016-09-02 14:25:53 +0300

    Update index.js

--- auth/auth-sms-login/index.js
+++ auth/auth-sms-login/index.js
@@ -164,7 +164,7 @@ function smsLogin(phone, code) {
   let identityId;
   let cognitoToken;
   let correctCode;
-  const plainPhone = phone.replace(/^[0]/g, '').replace(/\S/g, '');
+  const plainPhone = phone.replace(/^[0]/g, '').replace(/\s/g, '');
   if (!plainPhone || plainPhone.length < 4) {
     return Promise.reject(new Error('Invalid phone number'));
   }
