8beef20e8961ecd24272babb090ca45dd2b3e045
Lauri Svan <lauri.svan@iki.fi>
2016-09-02 19:58:52 +0300

    Fix: Autocomplete worked improperly

--- autocomplete/autocomplete-query/index.js
+++ autocomplete/autocomplete-query/index.js
@@ -3,45 +3,84 @@
 const Promise = require('bluebird');
 const bus = require('../../lib/service-bus');
 const MaaSError = require('../../lib/errors/MaaSError');
+const utils = require('../../lib/utils');
 
-function queryAutocomplete(event) {
-  if (!event.payload || Object.keys(event.payload).length === 0) {
-    return Promise.reject(new MaaSError('Missing or empty event payload', 401));
+/**
+ * Parses and validates the event input
+ * Contents: lat, lon, name, count (optional)
+ *
+ * @param {object} event The input event - see the contents above
+ * @return {Promise} Object of parsed parameters if success, MaaSError otherwise
+ */
+function parseAndValidateInput(event) {
+  if (typeof event.payload !== 'object') {
+    const message = `Invalid or missing event.payload '${JSON.stringify(event.payload)}'`;
+    return Promise.reject(new MaaSError(message, 400));
   }
 
+  const name = (utils.isEmptyValue(event.payload.name)) ? undefined : event.payload.name;
+  const lat = (utils.isEmptyValue(event.payload.lat)) ? undefined : parseFloat(event.payload.lat);
+  const lon = (utils.isEmptyValue(event.payload.lon)) ? undefined : parseFloat(event.payload.lon);
+  const count = (utils.isEmptyValue(event.payload.count)) ? 5 : parseInt(event.payload.count, 10);
+
+  if (typeof name !== 'string') {
+    return Promise.reject(new MaaSError(`Invalid of missing name value '${name}'`, 400));
+  }
+
+  if (!/[\d\s]*[\w]{3,}[\d\s]*/.test(name)) {
+    const message = `Name value too short or invalid '${name}', expecting 3 or more letters, with optionally spaces or numbers`;
+    return Promise.reject(new MaaSError(message, 400));
+  }
+
+  if (!Number.isFinite(lat) || lat < -90 || lat > 90) {
+    return Promise.reject(new MaaSError(`Invalid lat value '${lat}', must be -90 <= lat >= 90`, 400));
+  }
+
+  if (!Number.isFinite(lon) || lon < -180 || lon > 180) {
+    return Promise.reject(new MaaSError(`Invalid lon value '${lon}', must be -90 <= lon >= 90`, 400));
+  }
+
+  if (!Number.isInteger(count) || count < 1 || count > 100) {
+    return Promise.reject(new MaaSError(`Invalid count value '${count}', must be 0 < count >= 100`, 400));
+  }
+
+  return Promise.resolve({
+    name,
+    lat,
+    lon,
+    count,
+    hint: 'latlon',
+  });
+}
+
+function queryAutoComplete(parsed) {
   // Validate & set defaults
   const query = {
-    name: event.payload.name,
-    count: (parseInt(event.payload.count, 10)) === 0 ? 5 : event.payload.count,
-    lat: event.payload.lat,
-    lon: event.payload.lon,
+    name: parsed.name,
+    count: parsed.count,
+    lat: parsed.lat,
+    lon: parsed.lon,
+    hint: parsed.hint,
   };
 
-  // Inject input hints, typecast input
-  if (typeof query.lat === 'string' && typeof query.lon === 'string') {
-    query.lat = parseFloat(query.lat);
-    query.lon = parseFloat(query.lon);
-    query.hint = 'latlon';
-  } else {
-    query.hint = 'none';
-  }
-
-  if (typeof query.count === 'string') query.count = parseInt(query.count, 10);
-  console.info(query);
   return bus.call('MaaS-provider-here-autocomplete', query);
 }
 
 module.exports.respond = function (event, callback) {
 
-  return queryAutocomplete(event)
-    .then(results => {
-      callback(null, results);
-    })
-    .catch(err => {
-      console.info('This event caused error: ' + JSON.stringify(event, null, 2));
-      console.warn('Error:', err.stack);
+  return parseAndValidateInput(event)
+    .then(queryAutoComplete)
+    .then(results => callback(null, results))
+    .catch(_error => {
+      console.warn(`Caught an error: ${_error.message}, ${JSON.stringify(_error, null, 2)}`);
+      console.warn('This event caused error: ' + JSON.stringify(event, null, 2));
+      console.warn(_error.stack);
 
-      // TODO Process the error
-      callback(err);
+      if (_error instanceof MaaSError) {
+        callback(_error);
+        return;
+      }
+
+      callback(new MaaSError(`Internal server error: ${_error.toString()}`, 500));
     });
 };
