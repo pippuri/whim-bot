0d88513aa2f230d4ecb7e2e3682de80ca9b92f7a
Hieu Nguyen <blackevil245@gmail.com>
2016-09-02 16:01:35 +0300

    Fix malfunctioned count param

--- autocomplete/autocomplete-query/index.js
+++ autocomplete/autocomplete-query/index.js
@@ -4,40 +4,44 @@ const Promise = require('bluebird');
 const bus = require('../../lib/service-bus');
 const MaaSError = require('../../lib/errors/MaaSError');
 
-module.exports.respond = function (event, callback) {
-
+function queryAutocomplete(event) {
   if (!event.payload || Object.keys(event.payload).length === 0) {
     return Promise.reject(new MaaSError('Missing or empty event payload', 401));
   }
 
   // Validate & set defaults
-  return new Promise((resolve, reject) => {
-    const query = event.payload;
+  const query = {
+    name: event.payload.name,
+    count: (parseInt(event.payload.count, 10)) === 0 ? 5 : event.payload.count,
+    lat: event.payload.lat,
+    lon: event.payload.lon,
+  };
 
-    // Inject input hints, typecast input
-    if (typeof query.lat === 'string' && typeof query.lon === 'string') {
-      query.lat = parseFloat(query.lat);
-      query.lon = parseFloat(query.lon);
-      query.hint = 'latlon';
-    } else {
-      query.hint = 'none';
-    }
+  // Inject input hints, typecast input
+  if (typeof query.lat === 'string' && typeof query.lon === 'string') {
+    query.lat = parseFloat(query.lat);
+    query.lon = parseFloat(query.lon);
+    query.hint = 'latlon';
+  } else {
+    query.hint = 'none';
+  }
 
-    if (typeof query.count === 'string') {
-      query.count = parseInt(query.count, 10);
-    }
+  if (typeof query.count === 'string') query.count = parseInt(query.count, 10);
+  console.info(query);
+  return bus.call('MaaS-provider-here-autocomplete', query);
+}
 
-    resolve(query);
-  })
-  .then(query => bus.call('MaaS-provider-here-autocomplete', query))
-  .then(results => {
-    callback(null, results);
-  })
-  .catch(err => {
-    console.info('This event caused error: ' + JSON.stringify(event, null, 2));
-    console.warn('Error:', err.stack);
+module.exports.respond = function (event, callback) {
 
-    // TODO Process the error
-    callback(err);
-  });
+  return queryAutocomplete(event)
+    .then(results => {
+      callback(null, results);
+    })
+    .catch(err => {
+      console.info('This event caused error: ' + JSON.stringify(event, null, 2));
+      console.warn('Error:', err.stack);
+
+      // TODO Process the error
+      callback(err);
+    });
 };
