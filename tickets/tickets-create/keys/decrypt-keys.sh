c35addcb987dd49ffe325dc9664c59bb3410dc71
travis@maas.fi <travis>
2016-09-21 21:04:34 +0300

    Update tickets for alpha keys; update docs & scripts

--- tickets/tickets-create/keys/decrypt-keys.sh
+++ tickets/tickets-create/keys/decrypt-keys.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
-# James Nguyen
-# This script allows running in only dev and prod stage TODO add more
+#
+# Author: James Nguyen (james.nguyen@maas.fi)
+# TODO Describe what is the purpose of this script
 
 # Allow this script to run only in this folder
 if [[ ! $PWD =~ "maas-backend/tickets/tickets-create/keys" ]]
@@ -12,26 +13,25 @@ fi
 # -s for STAGE - stage in which this script deploy to
 while getopts "hd:s:" opt; do
   case $opt in
-    h  ) echo "Usage: bash decrypt-key.sh -s <STAGE>"; exit 1;;
+    h  ) echo "Usage: bash create-keys.sh -s <STAGE>"; exit 1;;
     s  ) export STAGE=$OPTARG;;
-    \? ) echo "Usage: bash decrypt-key.sh -s <STAGE>"; exit 1;;
+    \? ) echo "Usage: bash create-keys.sh -s <STAGE>"; exit 1;;
     :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
     *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
   esac
 done
 
-# Default stage to dev if input stage missing
-if [[ -z $STAGE ]] && [[ "$STAGE" != "prod" ]] && [[ "$STAGE" != "dev" ]];
+# Validate stage
+if [[ -z $STAGE ]] && [[ "$STAGE" != "dev" ]] && [[ "$STAGE" != "test" ]] && [[ "$STAGE" != "prod" ]] && [[ "$STAGE" != "alpha" ]];
   then
-    echo $STAGE
-    echo "No stage set or invalid stage (!prod && !dev), using dev"
-    unset STAGE
-    export STAGE=dev
+    echo "No stage set or invalid stage (should be dev, test, alpha or prod)."
+    echo "Usage: bash create-keys.sh -s <STAGE>"; exit 1;
   else
     echo "Using stage $STAGE"
 fi
 
-# Check if s-variables-$STAGE existed or not TODO implement more stages to this
+# Check if s-variables-$STAGE exists or not
+# TODO implement more stages to this
 if [[ ! -f ../../../_meta/variables/s-variables-$STAGE.json ]]
   then
     echo "No s-variables-$STAGE found"
@@ -41,9 +41,8 @@ if [[ ! -f ../../../_meta/variables/s-variables-$STAGE.json ]]
     echo "Extracting deploy secret"
     MAAS_TICKET_DEPLOY_SECRET=$(node -p -e "require('../../../_meta/variables/s-variables-$STAGE.json').MAAS_TICKET_DEPLOY_SECRET")
 
-    # Decrypt the file with the secret
-
-    echo "Decrypting files"
+    # Encrypt the file with the secret
+    echo "Encrypting files"
     openssl aes-256-cbc -pass "pass:$MAAS_TICKET_DEPLOY_SECRET" -in ./$STAGE-latest.js.asc -out ./$STAGE-latest.js -d -a
     openssl aes-256-cbc -pass "pass:$MAAS_TICKET_DEPLOY_SECRET" -in ./$STAGE-transitional.js.asc -out ./$STAGE-transitional.js -d -a
 
