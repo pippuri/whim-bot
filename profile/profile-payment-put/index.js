fc4b14a60bbb6521d0422e5f4ba5f4929084d9d6
Sami Pippuri <pippuri@gmail.com>
2016-09-22 15:29:39 +0300

    Addressing @laurisvan comments

--- profile/profile-payment-put/index.js
+++ profile/profile-payment-put/index.js
@@ -2,6 +2,7 @@
 
 const Promise = require('bluebird');
 const Subscription = require('../../lib/subscription-manager/index.js');
+const MaaSError = require('../../lib/errors/MaaSError');
 
 function updateUserData(event) {
   const identityId = event.identityId;
@@ -19,9 +20,7 @@ function updateUserData(event) {
     return Promise.reject(new Error('Invalid or missing identityId'));
   }
   return Subscription.updateUser(identityId, payload)
-    .then( _ => {
-      return Subscription.updateUserCreditCard(identityId, payload);
-    });
+    .then( _ => Subscription.updateUserCreditCard(identityId, payload) );
 }
 
 function wrapToEnvelope(resp, event) {
@@ -37,14 +36,11 @@ module.exports.respond = (event, callback) => {
   return updateUserData(event)
     .then(response => wrapToEnvelope(response, event))
     .then(envelope => callback(null, envelope))
-    .catch(error => {
+    .catch(_error => {
+      console.warn(`Caught an error:  ${_error.message}, ${JSON.stringify(_error, null, 2)}`);
       console.warn('This event caused error: ' + JSON.stringify(event, null, 2));
-      console.warn('Error: ' + JSON.stringify(error.toString(), null, 2));
-      if (error && error.hasOwnPropert('response')) {
-        console.info(error.response.toString());
-        callback(error.response.toString());
-      } else {
-        callback(error);
-      }
+      console.warn(_error.stack);
+
+      callback(new MaaSError(`Internal server error: ${_error.toString()}`, 500));
     });
 };
