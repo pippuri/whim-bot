282c36108697016ccc3f4ba6dc4322cce644c5fd
laurisvan <lauri.svan@iki.fi>
2016-09-27 15:52:23 +0300

    Add reference counting to init & cleanup

--- test/lib/index.js
+++ test/lib/index.js
@@ -3,6 +3,7 @@
 describe('Library', () => {
   require('./business-rule-engine');
   require('./geolocation');
+  require('./models');
   require('./subscription-manager');
   require('./trip');
   require('./tsp');
