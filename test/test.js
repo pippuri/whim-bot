1b8dc1fac197d62d3eeeffbe8942e1e92780f8e4
Juha Lehtomaki <jubilem@gmail.com>
2016-10-07 21:33:30 +0300

    trying to fix test.js env variables setup

--- test/test.js
+++ test/test.js
@@ -4,7 +4,8 @@ const Templates = (new (require('serverless'))()).classes.Templates;
 
 function loadEnvironment() {
   // The stages we can run tests against in priority order - always fallback to dev
-  const stages = ['dev', 'test'];
+  const stages = ['test'];
+  const dataCenter = 'euwest1';
 
   // Check the stage from the existence of meta sync file
   const stage = stages.find(stage => {
@@ -26,11 +27,19 @@ function loadEnvironment() {
 
   // Read the variables from the given path
   console.info(`Using stage '${stage}' variables for running the tests.`);
-  const values = require(`../_meta/variables/s-variables-${stage}.json`);
-  const variables = (new Templates(values, '../s-templates.json')).toObject();
-  for (let key of Object.keys(variables)) { // eslint-disable-line prefer-const
+
+  const valuesFile = require(`../_meta/variables/s-variables-${stage}.json`);
+  const dataCenterValuesFile = require(`../_meta/variables/s-variables-${stage}-${dataCenter}.json`);
+  const template = require('../s-templates.json').environment;
+
+  const values = new Templates(valuesFile);
+  values.setParents([new Templates(dataCenterValuesFile)]);
+  const allValues = values.toObject();
+  for (let key of Object.keys(template)) { // eslint-disable-line prefer-const
+    const variableKey = template[key].match(/^\${(.*)}$/);
+    template[key] = variableKey ? allValues[variableKey[1]] : undefined;
     // Use the environment if given, fallback to SLS meta
-    process.env[key] = process.env[key] ? process.env[key] : variables[key];
+    process.env[key] = process.env[key] ? process.env[key] : template[key];
   }
 }
 
