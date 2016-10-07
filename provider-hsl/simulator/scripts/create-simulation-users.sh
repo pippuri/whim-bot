744f7c109f81acb0d9863c9005cdc1997882ba0f
laurisvan <lauri.svan@iki.fi>
2016-10-07 08:42:21 +0300

    Remove dead/obsoleted code

--- provider-hsl/simulator/scripts/create-simulation-users.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Create a set of simulation users in the development environment
-# Simulation users have phone number prefix +292 (unallocated code)
-for ((i = 10000; i <= 10009; i++)); do
-  phone="+292$i"
-  echo "Registering simulation user $phone"
-  curl "https://api.dev.maas.global/auth/sms-login?code=292&phone=$phone"
-done
