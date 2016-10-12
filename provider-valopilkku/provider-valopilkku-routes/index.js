32b488401000a391b87376b8dfa2dfb264020f72
Konrad Markus <konker@iki.fi>
2016-10-12 14:36:18 +0300

    Amends to the valoplikku routes provider to reject an 'arriveBy' input, and to reject a mode which is not 'TAXI'

--- provider-valopilkku/provider-valopilkku-routes/index.js
+++ provider-valopilkku/provider-valopilkku-routes/index.js
@@ -1,30 +1,43 @@
 'use strict';
 
-const Promise = require('bluebird');
 const adapter = require('./adapter');
 const utils = require('../../lib/utils');
 const MaaSError = require('../../lib/errors/MaaSError');
 const TSPFactory = require('../../lib/tsp/TransportServiceAdapterFactory');
 
+const DEFAULT_MODE = 'TAXI';
+
+
 function parseAndValidateInput(event) {
-  if (event.leaveAt && event.arriveBy) {
-    return Promise.reject(new MaaSError('Both leaveAt and arriveBy provided.', 400));
+  if (!event.leaveAt) {
+    return Promise.reject(new MaaSError('No leaveAt parameter provided.', 400));
+  }
+
+  if (event.arriveBy) {
+    // "Note: Specifying arrival time is not supported for Valopilkku
+    // routing. Requesting will result in an error response."
+    return Promise.reject(new MaaSError('Here API does not support arriveBy for taxis.', 400));
+  }
+
+  if (!utils.isEmptyValue(event.mode)) {
+    console.warn('WARNING: event contains `mode` property, this will be ignored. Did you mean `modes`?');
+  }
+
+  const modes = utils.isEmptyValue(event.modes) ? [DEFAULT_MODE] : event.modes.split(',');
+  if (modes.indexOf(DEFAULT_MODE) === -1) {
+    return Promise.reject(new MaaSError(`Unsupported mode(s): ${event.modes}`, 400));
   }
 
   const from = (utils.isEmptyValue(event.from)) ? undefined : event.from.split(',').map(parseFloat);
   const to = (utils.isEmptyValue(event.to)) ? undefined : event.to.split(',').map(parseFloat);
   const startTime = (utils.isEmptyValue(event.leaveAt)) ? Date.now() + 5000 : parseInt(event.leaveAt, 10);
-  const endTime = (utils.isEmptyValue(event.leaveAt)) ? undefined : parseInt(event.arriveBy, 10);
 
   const ret = {
-    mode: 'TAXI',
+    mode: DEFAULT_MODE,
     from: from,
     to: to,
     startTime: startTime,
   };
-  if (endTime) {
-    ret.endTime = endTime;
-  }
 
   return Promise.resolve(ret);
 }
