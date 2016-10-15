eb4aaba07f5f2ad3d92a64844694c224392fe837
laurisvan <lauri.svan@iki.fi>
2016-10-15 15:43:29 +0300

    Permit new user creation even with existing Chargebee account

--- test/lib/business-objects/Profile.js
+++ test/lib/business-objects/Profile.js
@@ -5,10 +5,27 @@ const samplePlan = require('./chargebee-fi-whim-light.json');
 const Profile = require('../../../lib/business-objects/Profile');
 
 describe('Profile', () => {
+  const testUserIdentity = 'eu-west-1:00000000-dead-dead-eaea-000000000000';
+  const testUserPhone = '+358417556933';
+
   describe('_toSubscription', () => {
     it('Converts features to agencyIds', () => {
       const converted = Profile._toSubscription(samplePlan);
       expect(converted.agencies).to.include('HSL');
+    });
+  });
+
+  describe.skip('TODO create', () => {
+    it('Creates a new profile for an user that alraedy exists', () => {
+      return Profile.create(testUserIdentity, testUserPhone)
+        .then(profile => {
+          return profile;
+        })
+        .catch(error => {
+          console.log('Caught an error', error.message);
+          console.log(JSON.stringify(error, null, 2));
+          console.log(error.stack);
+        });
     });
   });
 });
