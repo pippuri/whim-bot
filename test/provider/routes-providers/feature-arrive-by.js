6ac8526202324044bbc1e3a966a5fc32c6ff4bc5
laurisvan <lauri.svan@iki.fi>
2016-10-27 19:06:52 +0300

    Add date/time information to tests for better traceability

--- test/provider/routes-providers/feature-arrive-by.js
+++ test/provider/routes-providers/feature-arrive-by.js
@@ -8,12 +8,15 @@ const validator = require('../../../lib/validator');
 
 module.exports = function (lambda) {
 
-  describe('arriveBy request', () => {
+  // Monday one week forward around five
+  const arriveBy = moment().tz('Europe/Helsinki').day(8).hour(17).minute(0).second(0);
+
+  describe(`arriveby request at ${arriveBy.format()}`, () => {
 
     const event = {
       from: '60.1684126,24.9316739', // SC5 Office
       to: '60.170779,24.7721584', // Gallows Bird Pub
-      arriveBy: '' + moment().tz('Europe/Helsinki').day(8).hour(17).valueOf(), // Monday one week forward around five
+      arriveBy: `${arriveBy.valueOf()}`,
     };
 
     let error;
