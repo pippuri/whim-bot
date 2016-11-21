2a52062812782ab8baf84e2b8eca880f0f08807b
Hieu Nguyen <blackevil245@gmail.com>
2016-11-21 18:31:32 +0200

    Refactor Transaction class

--- scripts/migrations/20161119153600_Transaction-log.js
+++ scripts/migrations/20161119153600_Transaction-log.js
@@ -4,14 +4,13 @@ exports.up = function (knex, Promise) {
   return knex.schema
     .createTable('TransactionLog', table => {
       // Identifiers
-      table.increments('id').primary();
+      table.increments('id').primary().notNullable().unique();
       table.string('identityId').index().notNullable();
       table.string('message').notNullable();
-      table.integer('value');
-      table.json('associations').notNullable();
+      table.integer('value').notNullable();
+      table.jsonb('associations').notNullable();
 
-      table.timestamp('created').index().notNullable()
-        .defaultTo(knex.raw('now()'));
+      table.timestamp('created').index().notNullable().defaultTo(knex.raw('now()'));
       // Note: Transaction log is not intended to be modified after creation.
       // This is here for the sake of consistency.
       table.timestamp('modified');
