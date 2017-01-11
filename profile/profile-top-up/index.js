6b658da0f8bcb4460d9e96b612f0d7034a2b4538
laurisvan <lauri.svan@iki.fi>
2017-01-11 12:02:44 +0200

    Update top-up endpoint for new API (webhook still needed for balance update)

--- profile/profile-top-up/index.js
+++ profile/profile-top-up/index.js
@@ -3,8 +3,9 @@
 const MaaSError = require('../../lib/errors/MaaSError');
 const models = require('../../lib/models');
 const Profile = require('../../lib/business-objects/Profile');
+const Pricing = require('../../lib/business-objects/Pricing');
 const Promise = require('bluebird');
-const SubscriptionMgr = require('../../lib/subscription-manager');
+const SubscriptionManager = require('../../lib/business-objects/SubscriptionManager');
 const Transaction = require('../../lib/business-objects/Transaction');
 
 /**
@@ -22,9 +23,9 @@ function parseAndValidateInput(event) {
   }
 
   const params = event.payload;
-  const productId = params.productId;
   const points = parseInt(params.amount, 10);
-  const limit = parseFloat(params.chargeOK, 10);
+  const limit = parseFloat(params.price, 10);
+  const currency = params.currency;
 
   if (!(points > 0)) {
     return Promise.reject(new MaaSError(`Invalid or missing amount '${points}'`, 400));
@@ -34,64 +35,58 @@ function parseAndValidateInput(event) {
     return Promise.reject(new MaaSError(`Invalid or missing chargeOK '${limit}'`, 400));
   }
 
-  if (typeof productId !== 'string') {
-    return Promise.reject(new MaaSError('Missing productId', 400));
+  if (typeof currency !== 'string') {
+    return Promise.reject(new MaaSError('Missing currency', 400));
   }
 
   return Promise.resolve({
     identityId,
-    productId,
     points,
     limit,
+    currency,
   });
 }
 
-function confirmCharge(identityId, productId, points, limit) {
-  return SubscriptionMgr.getAddonById(productId)
-  .then(response => {
-    const cost = response.addon.price * response.addon.meta_data.pointGrant * points;
+function confirmCharge(identityId, points, limit, currency) {
+  return Pricing.convertPointsToCost(points, currency)
+    .then(cost => {
+      if (cost > limit) {
+        const m = `Could not charge ${cost} ${currency} for ${points}p top-up, user permitted only ${limit} ${currency}`;
+        throw new MaaSError(m, 403);
+      }
 
-    // Validate the user has OKd the change
-    if (!(cost <= limit)) {
-      const message = `Requested to charge '${cost}' cents, but the limit was '${limit}'`;
-      return Promise.reject(new MaaSError(message, 403));
-    }
-
-    console.info(`Charge '${cost}' cents confirmed for ${identityId}`);
-    return {
-      identityId,
-      productId,
-      points,
-      cost,
-      limit,
-    };
-  });
+      console.info(`Top-up charge '${cost}' ${currency} confirmed for ${identityId}`);
+      return {
+        identityId,
+        currency,
+        points,
+        cost,
+        limit,
+      };
+    });
 }
 
 /**
- * make the addon product purchase and update profile with new points tally
+ * Make the addon product purchase and update profile with new points tally
  */
-function makePurchase(identityId, transaction, productId, cost, points) {
+function makePurchase(identityId, transaction, points) {
   return Profile.retrieve(identityId)
   .then(profile => {
     if (!profile) {
       throw new MaaSError('User not found', 404);
     }
     // delegate the purchase to subscription manager / charge manager
-    return SubscriptionMgr.makePurchase(identityId, productId, cost);
-  })
-  .then(purchase => {
-    if (purchase.status === 'paid') {
-      // update profile with the points the successful purchase grants
-      return Profile.increaseBalance(identityId, points, transaction)
-        .then(() => Promise.resolve(purchase));
-    }
-    return Promise.reject(new MaaSError(`Purchase failed on ChargeBee: ${purchase}`, 500));
+    const update = {
+      addons: [{
+        id: 'fi-whim-top-up',
+        quantity: points,
+      }],
+    };
+    return SubscriptionManager.updateSubscription(update, identityId, identityId, true, false);
   });
 }
 
 module.exports.respond = function (event, callback) {
-
   const transaction = new Transaction(event.identityId);
   let payload;
 
@@ -102,10 +97,10 @@ module.exports.respond = function (event, callback) {
       return models.Database.init()
         .then(() => transaction.start());
     })
-    .then(() => confirmCharge(event.identityId, payload.productId, payload.points, payload.limit))
-    .then(confirmed => makePurchase(confirmed.identityId, transaction, confirmed.productId, confirmed.cost, confirmed.points))
+    .then(() => confirmCharge(event.identityId, payload.points, payload.limit, payload.currency))
+    .then(confirmed => makePurchase(confirmed.identityId, transaction, confirmed.points))
     .then(purchase => {
-      return transaction.commit(`Topup ${payload.points}p`)
+      return transaction.commit(`Topup order of ${payload.points}p`)
         .then(() => Profile.retrieve(event.identityId))
         .then(profile => {
           return Object.assign({}, purchase, { profile: profile });
--- test/lib/business-objects/Pricing.js
+++ test/lib/business-objects/Pricing.js
@@ -6,25 +6,25 @@ const Pricing = require('../../../lib/business-objects/Pricing');
 describe('Pricing', () => {
   describe('convertCostToPoints', () => {
     it('should convert 1€ to 20 points', () => {
-      Pricing.convertCostToPoints(1, 'EUR')
+      return Pricing.convertCostToPoints(1, 'EUR')
         .then(points => expect(points).to.equal(20));
     });
 
-    it('should round up to the nearest integer with no decimals', () => {
-      Pricing.convertCostToPoints(9.99, 'EUR')
-        .then(points => expect(points.toString()).equal('200'));
+    it('should round down to the nearest integer with no decimals', () => {
+      return Pricing.convertCostToPoints(9.99, 'EUR')
+        .then(points => expect(points.toString()).equal('199'));
     });
   });
 
   describe('convertPointsToCost', () => {
     it('should convert 1 point to 5 cents', () => {
-      Pricing.convertPointsToCost(1, 'EUR')
+      return Pricing.convertPointsToCost(1, 'EUR')
         .then(euros => expect(euros).to.equal(0.05));
     });
 
     it('should round the currencies to two decimals', () => {
-      Pricing.convertPointsToCost(0.1, 'EUR')
-        .then(euros => expect(euros.toString()).to.equal(0.01));
+      return Pricing.convertPointsToCost(0.1, 'EUR')
+        .then(euros => expect(euros.toString()).to.equal('0.01'));
     });
   });
 });
