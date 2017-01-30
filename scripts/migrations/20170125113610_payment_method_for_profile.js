192350e8b58906fb035dd4fcce35b925b1c49879
Konrad Markus <konker@iki.fi>
2017-01-30 17:44:23 +0200

    Amend migration to set current user's paymentMethod.valid flag to 'true'

--- scripts/migrations/20170125113610_payment_method_for_profile.js
+++ scripts/migrations/20170125113610_payment_method_for_profile.js
@@ -4,9 +4,17 @@
 exports.up = function (knex) {
   return knex.schema
     .table('Profile', table => {
-      table.jsonb('paymentMethod')
-           .notNullable()
-           .defaultsTo(knex.raw('\'{}\'::JSONB'));
+      table
+        .jsonb('paymentMethod')
+        .notNullable()
+        .defaultsTo(knex.raw('\'{"type":"unknown", "valid":false}\'::JSONB'));
+    })
+    .then(() => {
+      // Update current users to set the valid flag for backwards compatability
+      return knex('Profile')
+        .update({
+          paymentMethod: '{ "type":"unknown", "valid":true }',
+        });
     });
 };
 
