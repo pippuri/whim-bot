76ce87e83d2263a3a6a4e0c9c2c6eec6c98a3616
Konrad Markus <konker@iki.fi>
2017-02-06 14:54:32 +0200

    Add script for migrating the Profile.paymentMethod field for existing users using Chargebee information

---
+++ scripts/update-profile-payment-method-from-chargebee.js
@@ -0,0 +1,149 @@
+'use strict';
+
+// Use mock lambdas (has to be set before requiring service bus)
+process.env.USE_MOCK_LAMBDA = true;
+
+const bus = require('../lib/service-bus');
+const models = require('../lib/models');
+const Promise = require('bluebird');
+const script = require('commander');
+
+const CUSTOMER_RETRIEVE = 'MaaS-subscriptions-customer-retrieve';
+
+const MISSING_PAYMENT_METHOD = {
+  type: 'unknown',
+  valid: false,
+};
+
+const ANSI_RESET = '\u001B[0m';
+const ANSI_RED = '\u001B[31m';
+const ANSI_GREEN = '\u001B[32m';
+const ANSI_YELLOW = '\u001B[33m';
+const ANSI_CYAN = '\u001B[36m';
+
+const green = s => `${ANSI_GREEN}${s}${ANSI_RESET}`;
+const yellow = s => `${ANSI_YELLOW}${s}${ANSI_RESET}`;
+const red = s => `${ANSI_RED}${s}${ANSI_RESET}`;
+const cyan = s => `${ANSI_CYAN}${s}${ANSI_RESET}`;
+
+const invalidPaymentMethod = profile => (
+  !profile.paymentMethod ||
+  !profile.paymentMethod.type ||
+  (profile.paymentMethod.type === 'unknown' &&
+   profile.paymentMethod.valid)
+);
+
+// Read command line arguments
+script.version('0.0.1')
+  .description('Update postgres Profile.paymentMethod field based on Chargebee customer data')
+  .option('-s, --stage [stage]', 'Choose a stage *required*')
+  .parse(process.argv);
+
+if (!script.stage) {
+  console.log('You must specify a table');
+  script.outputHelp();
+  process.exit(1);
+} else {
+  console.log('                     Updating Profilles                     ');
+  console.log('============================================================');
+  console.log('');
+}
+
+
+// Read in the serverless environment
+const variableFile = require(`../_meta/variables/s-variables-${script.stage}.json`);
+// eslint-disable-next-line
+for (const props in variableFile) {
+  process.env[props] = variableFile[props];
+}
+
+
+// Initialize some counters for keeping track of activity
+let postgresProfilesTotal = 0;
+let postgresProfilesUpdated = 0;
+let postgresProfilesSkipped = 0;
+let postgresProfilesSkippedNotFound = 0;
+let postgresProfilesFailed = 0;
+
+
+// Process an individual Profile, updating `paymentMethod` if necessary
+function processProfile(profile) {
+  console.log('\n------------------------------------------------------------');
+  console.log('Profile: ', cyan(profile.identityId));
+  postgresProfilesTotal += 1;
+
+  // Warn about obvious test Profiles
+  if (profile.identityId.indexOf('cafe-cafe') !== -1 ||
+      profile.identityId.indexOf('dead-dead') !== -1) {
+
+    console.log(yellow('WARNING Possible test profile'));
+  }
+
+  // Check if paymentMethod is already valid
+  if (!invalidPaymentMethod(profile)) {
+    console.log('Payment method ', green('VALID'));
+    console.log(profile.paymentMethod);
+    console.log(green('SKIPPED'));
+    postgresProfilesSkipped += 1;
+    return Promise.resolve();
+  }
+
+  // paymentMethod invalid, so proceed
+  console.log('Payment method incomplete');
+  console.log(profile.paymentMethod);
+  console.log('Updating...');
+
+  // Fetch the customer information
+  const payload = {
+    customerId: profile.identityId,
+    userId: profile.identityId,
+  };
+  return bus.call(CUSTOMER_RETRIEVE, payload)
+    .then(customer => {
+      // Extract the customer paymentMethod or use the default one
+      // which indicates that no payment method is available
+      const paymentMethod = customer.paymentMethod ? customer.paymentMethod : MISSING_PAYMENT_METHOD;
+      console.log('Updating to: ', paymentMethod);
+
+      // Update the Profile
+      return models.Profile
+        .query()
+        .patch({ paymentMethod })
+        .where('identityId', '=', profile.identityId)
+        .then(() => {
+          console.log(green('UPDATED'));
+          postgresProfilesUpdated += 1;
+        });
+    })
+    .catch(err => {
+      if (err.status === 404) {
+        console.log('Unexpected error:', profile.identityId, err);
+        console.log(red('FAILED'));
+        postgresProfilesFailed += 1;
+      } else {
+        postgresProfilesSkippedNotFound += 1;
+        console.log(yellow('SKIPPED 404'));
+      }
+    });
+}
+
+
+// Intitialize the database
+models.Database.init()
+  // Fetch all Profiles in the database
+  .then(() => models.Profile.query())
+  .then(profiles => {
+    // Call `processProfile` for each Profile sequentially
+    return Promise.each(profiles, processProfile);
+  })
+  .then(() => {
+    // Print out summary of activity
+    console.log('\n\n');
+    console.log(`Postgres profiles in total:\t${postgresProfilesTotal}`);
+    console.log(`Postgres profiles updated:\t${postgresProfilesUpdated}`);
+    console.log(`Postgres profiles skipped:\t${postgresProfilesSkipped}`);
+    console.log(`Postgres profiles 404:\t\t${postgresProfilesSkippedNotFound}`);
+    console.log(`Postgres profiles failed:\t${postgresProfilesFailed}`);
+  })
+  .finally(() => models.Database.cleanup());
+
